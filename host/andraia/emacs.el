90b05072d9f3c6ed052623e05fd2602f8ace556d
mrled <mrled@64fab21a-7213-0410-900d-ec9aa1f7df7c>
2007-06-16 18:05:51 +0000

    emacs and emacs-muse stuff on andraia only
    git-svn-id: file:///home/mrled/opt/rcs/dhd@136 64fab21a-7213-0410-900d-ec9aa1f7df7c

--- host/andraia/emacs.el
+++ host/andraia/emacs.el
@@ -1,38 +1,32 @@
+; andraia's host-specific emacs init file
+
+; misc stuff
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/")
+;(autoload 'psvn "psvn" "SVN for Emacs")
+;(autoload 'org-install "org-install" "Organization mode for Emacs")
+;(autoload 'vm "vm" "A more MUA-like MUA for Emacs")
 
-(autoload 'psvn "psvn" "SVN for Emacs")
-(autoload 'org-install "org-install" "Organization mode for Emacs")
-(autoload 'vm "vm" "A more MUA-like MUA for Emacs")
+; emacs-muse
+(if (file-exists-p 
+  (setq custom-file (concat "~/doc/remote/dhd/host/" host-name "/emacs.muse.el")))
+    (load-file custom-file))
 
-;(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/muse/")
-(autoload 'muse "muse" "Wiki mode for Emacs")
-(require 'muse)
-(require 'muse-mode)
-(require 'muse-project)
-(require 'muse-html)
-(require 'muse-latex)
-(require 'muse-texinfo)
-(require 'muse-docbook)
-(require 'muse-book)
-(require 'muse-blosxom)
 
 ;; ceded (emacs ide-like features) stuff
 ;(if (file-exists-p (setq cedet-file (concat home-load-path "cedet/common/cedet.elc")))
 ;  (load-file cedet-file)
 ;  (semantic-load-enable-code-helpers))
 
-;; lisp programming stuff
-;; (if (file-exists-p (setq slime-path (concat home-load-path "slime/")))
-;;   (add-to-list 'load-path slime-path)
-;;   (setq inferior-lisp-program "/usr/local/bin/openmcl")
-;;   (require 'slime)
-;;   (slime-setup))
-
+; slime
+; (if (file-exists-p (setq slime-path (concat home-load-path "slime/")))
+;   (add-to-list 'load-path slime-path)
+;   (setq inferior-lisp-program "/usr/local/bin/openmcl")
+;   (require 'slime)
+;   (slime-setup))
 (add-to-list 'load-path "~/opt/emacs/slime-2.0/")
 (setq inferior-lisp-program "/usr/local/bin/openmcl")
 (require 'slime)
 (slime-setup)
-
 
 (autoload 'slime "slime"
           "Start an inferior^_superior Lisp and connect to its Swank server."
