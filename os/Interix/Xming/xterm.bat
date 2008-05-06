8a8803f39edef343d9c858c852dd6280785cebfc
mrled <mrled@64fab21a-7213-0410-900d-ec9aa1f7df7c>
2008-05-06 21:02:25 +0000

    added Xming shits
    git-svn-id: file:///home/mrled/opt/rcs/dhd@256 64fab21a-7213-0410-900d-ec9aa1f7df7c

---
+++ os/Interix/Xming/xterm.bat
@@ -0,0 +1,2 @@
+rem ember that this file belongs in C:\Program Files\Xming
+C:\WINDOWS\system32\posix.exe /u /c /bin/ksh -c "/usr/local/bin/xterm -bg black -fg white -sl 10000 -ls -fn -misc-profontwindows-medium-r-normal--12-0-0-0-m-0-iso8859-1 &"
