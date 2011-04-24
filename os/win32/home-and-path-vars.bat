47a42e7778e369618764eac7d75e80ebd814154e
mrled.BONNIE <micah@micahrl.com>
2011-04-23 20:18:34 -0500

    several new files, including a working batch file (no powershell required) which sets your %HOME% variable, and will set %PATH% based on which directories exist when you run it (you still need to run it manually though

---
+++ os/win32/home-and-path-vars.bat
@@ -0,0 +1,52 @@
+@echo off
+
+rem Setting the %HOME% variable to %USERPROFILE%
+rem Setting the %PATH% variable 
+
+
+:s_home
+rem Note that this will copy the VALUE of %USERPROFILE% to %HOME%
+rem so if %USERPROFILE% changes, %HOME% will not unless you do it manually
+setx HOME %USERPROFILE%
+:s_home_end
+
+
+:s_path
+rem If you want to set the system %PATH%, use the -m flag like: 
+rem     setx PATH "C:\opt\bin;C:\opt\ntemacs\bin;" 
+
+rem Note: Windows does not require double-quotes around entries in %PATH% --
+rem   even entries with spaces. In fact, when I call the setx argument, it
+rem   appears to strip them. However. I couldn't figure out a way to 
+rem   make this script work without adding them. 
+rem   (cmd syntax is bullshit.)
+
+set MYPATH=
+call :s_check_exists C:\opt\local\bin
+call :s_check_exists C:\opt\ntemacs24\bin
+call :s_check_exists C:\opt\git\bin
+call :s_check_exists C:\Program Files\PuTTY
+call :s_check_exists C:\Program Files (x86)\PuTTY
+
+setx PATH %MYPATH%
+set MYPATH=
+
+goto :s_path_end
+
+rem Note that I couoldn't do a nested if statement without all these 
+rem    `call` statements. 
+rem   (cmd syntax is bullshit.)
+:s_check_exists
+if exist "%*" call :s_exists_yes %*
+goto :eof
+
+:s_exists_yes
+rem echo Adding: %*
+if defined MYPATH (
+    set MYPATH=%MYPATH%;"%*"
+) else (
+    set MYPATH="%*"
+)
+goto :eof
+
+:s_path_end
