45dd950a6f8d0678712cdf08b4026a1f9cda2510
Micah R Ledbetter/AndrAIa <vlack@vlack.com>
2011-05-14 13:32:58 -0500

    added debug statements to smb2.sh

--- opt/bin/smb2.sh
+++ opt/bin/smb2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-_smbclient=smbclient
-_smbclientopts=
-#_smbclientopts="--authentication-file=~mledbetter/.netrc.smbclient"
+
+smbclient=smbclient
+smbclientopts=
+#smbclientopts="--authentication-file=~mledbetter/.netrc.smbclient"
 thiscmd=`basename $0`
 USAGE="Usage: '$thiscmd [cp|ls|help|echo]' [URL]."
 URLFORMAT="URLs must be in form [user@]host[:share[/path/to/somewhere]]."
+debug=1
+# if [ "$debug" -eq "1" ]; then echo omg; fi
 
 _parse() {
     wholestring="$*"
@@ -19,23 +22,33 @@ _parse() {
 
     # first determine if we have to parse a ":share[/path]" string
     echo $wholestring | grep : >/dev/null
-    if [ "$?" -eq "0" ]; then #"share[/path]" present
+    if [ "$?" -eq "0" ]; then 
+        if [ "$debug" -eq "1" ]; then echo "Determined that share/path is present" ; fi
         userserver=`echo $wholestring | awk 'BEGIN {FS=":"}; {print $1}'`
+        if [ "$debug" -eq "1" ]; then echo userserver = $userserver; fi
         sharepath=`echo $wholestring | awk 'BEGIN {FS=":"}; {print $2}'`
+        if [ "$debug" -eq "1" ]; then echo sharepath = $sharepath; fi
         # $userserver contains something like "user@server" or just "server"
         # $sharepath contains something like "share/path" or just "share"
 
         # parse the "share[/path]" string. Do we have a path, or just a share?
         echo $sharepath | grep \/ >/dev/null 
-        if [ "$?" -eq "0" ]; then #path present
+        if [ "$?" -eq "0" ]; then 
+            if [ "$debug" -eq "1" ]; then echo "Determined that remotepath is present"; fi
             share=`echo $sharepath | awk 'BEGIN {FS="/"}; {print $1}'`
+            if [ "$debug" -eq "1" ]; then echo share = $share; fi
             remotepath=`echo $sharepath | sed "s/$share\///"`
-            _smbclientopts="$_smbclientopts -D \"$remotepath\""
-        else #no path present, so the whole string is just the share
-            share=$sharepath
-        fi
+            if [ "$debug" -eq "1" ]; then echo remotepath = $remotepath; fi
 
+            smbclientopts="$smbclientopts -D \"$remotepath\""
+            if [ "$debug" -eq "1" ]; then echo smbclientopts = $smbclientopts; fi
+        else
+            if [ "$debug" -eq "1" ]; then echo "Determined that remotepath is not present"; fi
+            share=$sharepath
+            if [ "$debug" -eq "1" ]; then echo share = $share; fi
+        fi
     else 
+        if [ "$debug" -eq "1" ]; then echo "Determined that share/path is not present" ; fi
         # first make sure the user didn't accidentally give you a url like
         # user@server/share/path, which we don't support yet
         echo $wholestring | grep \/ >/dev/null 
@@ -50,24 +63,25 @@ _parse() {
 
     # now determine if we have a "user@server" string, or just a "server" string
     echo $userserver | grep \@ >/dev/null
-    if [ "$?" -eq "0" ]; then #username present
+    if [ "$?" -eq "0" ]; then 
+        if [ "$debug" -eq "1" ]; then echo "Username is present" ; fi
         username=`echo $userserver | awk 'BEGIN {FS="@"}; {print $1}'`
-        _smbclientopts="$_smbclientopts --user=$username"
+        if [ "$debug" -eq "1" ]; then echo username = $username; fi        
+        smbclientopts="$smbclientopts --user=$username"
+        if [ "$debug" -eq "1" ]; then echo smbclientopts = $smbclientopts; fi        
         server=`echo $userserver | awk 'BEGIN {FS="@"}; {print $2}'`
+        if [ "$debug" -eq "1" ]; then echo server = $server; fi        
     else #no username present so the whole string is just the server
+        if [ "$debug" -eq "1" ]; then echo "Username is not present" ; fi
         server=$userserver
+        if [ "$debug" -eq "1" ]; then echo server = $server ; fi
     fi
 }
 _echo() {
-    _parse $*
     echo "User:  $username"
     echo "Host:  $server"
     echo "Share: $share"
     echo "Path:  $remotepath"
-}
-_put() {
-}
-_get() {
 }
 _cp() {
     # figure out which argument is remote
@@ -88,7 +102,7 @@ _cp() {
     elif ! [ -z $arg1remote ]; then #arg1 is remote; we are downloading
         remotefile="$2"
         _parse "$1"
-        $_smbclient $_smbclientopts //$server/$share <<EOF
+        $smbclient $smbclientopts //$server/$share <<EOF
 prompt
 mget $remotefile
 exit
@@ -96,7 +110,7 @@ EOF
     elif ! [ -z $arg2remote ]; then #arg2 is remote; we are uploading
         localfile="$1"
         _parse "$2"
-        $_smbclient $_smbclientopts //$server/$share <<EOF
+        $smbclient $smbclientopts //$server/$share <<EOF
 prompt
 mput $localfile
 exit
@@ -110,10 +124,13 @@ EOF
 }
 _ls() {
     _parse $*
-    if [ -z "$share" ]; then #no share, just list shares
-        $_smbclient $_smbclientopts -L $server
-    else #there is a remotepath, view it
-        $_smbclient $_smbclientopts //$server/$share <<EOF
+    if [ "$debug" -eq "1" ]; then _echo ; fi
+    if [ -z "$share" ]; then 
+        if [ "$debug" -eq "1" ]; then echo "no share provided, just list shares" ; fi        
+        $smbclient $smbclientopts -L $server
+    else 
+        if [ "$debug" -eq "1" ]; then echo "a remote path was provided; list it" ; fi        
+        $smbclient $smbclientopts //$server/$share <<EOF
 ls
 exit
 EOF
@@ -129,10 +146,10 @@ _help() {
 # first parse the main command, use shift to eat that command, and pass the rest of
 # the arguments on to the appropriate function. 
 case $1 in 
-    "ls")   shift; _ls   $*; exit;;
-    "cp")   shift; _cp   $*; exit;;
-    "echo") shift; _echo $*; exit;;
+    "ls") shift; _ls $*; exit;;
+    "cp") shift; _cp $*; exit;;
 
+    "echo") shift; _parse $*; _echo; exit;;
     "help") shift; _help; exit;;
     *) echo $USAGE; exit;;
 esac
