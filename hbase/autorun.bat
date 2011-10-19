5bdc50fd1f5716d96c430f7916cdc15fd1ab9b26
Micah@MICAH-PC <micah@micahrl.com>
2011-10-19 17:32:56 -0500

    commented out everything in autorun.bat because in some cases that run cmd - such as when emacs runs markdown-command - it would give error messages related to the 'doskey' lines. I will figure something else out later.

--- hbase/autorun.bat
+++ hbase/autorun.bat
@@ -3,8 +3,8 @@
 rem - To use this you must set the AutoRun registry key:
 rem - reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /d "%USERPROFILE%\.dhd\hbase\autorun.bat"
 
-doskey omg=echo wtf $*
-doskey wh=which $* /a
-doskey sourceautorun=%USERPROFILE%\.dhd\hbase\autorun.bat
-doskey sourceautoruns=%USERPROFILE%\.dhd\hbase\autorun.bat
-doskey markdown=C:\strawberry\perl\bin\perl.exe %USERPROFILE%\.dhd\opt\bin\Markdown.pl
+rem doskey omg=echo wtf $*
+rem doskey wh=which $* /a
+rem doskey sourceautorun=%USERPROFILE%\.dhd\hbase\autorun.bat
+rem doskey sourceautoruns=%USERPROFILE%\.dhd\hbase\autorun.bat
+rem doskey markdown=C:\strawberry\perl\bin\perl.exe %USERPROFILE%\.dhd\opt\bin\Markdown.pl
