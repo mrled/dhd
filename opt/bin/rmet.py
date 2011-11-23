025c2ac3830ad0f949139147220128e8541e092b
Micah@MICAH-PC <micah@micahrl.com>
2011-11-23 16:31:12 -0600

    Reformatted some of rmet.py so that it is easier to read the code

--- opt/bin/rmet.py
+++ opt/bin/rmet.py
@@ -1,42 +1,69 @@
 #!/usr/bin/python3 
 
-import sys
+import sys, os, time, subprocess, re
 
 def mungeoutput(output):
-    headerfooter = re.compile("(id\tSubject\n)|(\n$)") #the header is id\tSubject\n and the footer is just an extra \n
-    linebegin = re.compile("^|\n") #the begining of the line OR of the whole block of text
+    #match a header of id\tSubject\n and a footer of just an extra \n
+    headerfooter = re.compile("(id\tSubject\n)|(\n$)") 
+
+    #match the begining of the line OR of the whole block of text
+    linebegin = re.compile("^|\n") 
+
+    #match an eof
     eof = re.compile("$")
+
+    #match a ticket line, consisting of a ticket number, a tab, and a title
+    #like "1234	Fix some horrifying problem by yesterday"
     ticketline = re.compile("([0-9]*)\t(.*)")
-    
-    output = str(output, "utf-8") #required b/c it comes to us from subprocess.check_output which returns a bytes object
+
+    #required b/c it comes to us from subprocess.check_output which returns a bytes object
+    output = str(output, "utf-8") 
+
+    ##now process the regexps
+
+    #nuke header and footer
     output = re.sub(headerfooter, r"", output)
+
+    #change line beginings to have bullets in front of them
     output = re.sub(linebegin, r"\n- ", output)
+
+    #add an extra line at the end for... some reason
     output = re.sub(eof, r"\n", output)
 
+    #make the ticket line into a Markdown link
     output = re.sub(ticketline, r"[\2](https://bugs.neuric.internal/Ticket/Display.html?id=\1)", output)
 
     return output
 
-import os, time, subprocess, re
 
 rtbin="/opt/rt40/bin/rt"
-
 today=time.strftime("%Y%m%d")
 
+# By default, check the last 6 days, but use the first argument if present
 if (len(sys.argv) <= 1):
     numdays=6
 else:
     numdays=sys.argv[1]
-
 updated="Updated >= '" + str(numdays) + " days ago'"
 
-resolvedtickets=mungeoutput(subprocess.check_output([rtbin, "ls", "-f", "Subject", "-o", "-Created", 
-                                                     "Queue = '5' AND Status = 'resolved' AND " + updated]))
-modifiedtickets=mungeoutput(subprocess.check_output([rtbin, "ls", "-f", "Subject", "-o", "-Created", 
-                                                     "Queue = '5' AND Status != 'resolved' AND Status != 'rejected' AND " + updated]))
-rejectedtickets=mungeoutput(subprocess.check_output([rtbin, "ls", "-f", "Subject", "-o", "-Created", 
-                                                     "Queue = '5' AND Status = 'rejected' AND " + updated]))
 
+# Find resolved tickets
+cmd=[rtbin, "ls", "-f", "Subject", "-o", "-Created", 
+     "Queue = '5' AND Status = 'resolved' AND " + updated]
+resolvedtickets=mungeoutput(subprocess.check_output(cmd))
+
+# Find modified tickets
+cmd=[rtbin, "ls", "-f", "Subject", "-o", "-Created", 
+     "Queue = '5' AND Status != 'resolved' AND Status != 'rejected' AND " + updated]
+modifiedtickets=mungeoutput(subprocess.check_output(cmd))
+
+# Find rejected tickets
+cmd=[rtbin, "ls", "-f", "Subject", "-o", "-Created",
+     "Queue = '5' AND Status = 'rejected' AND " + updated]
+rejectedtickets=mungeoutput(subprocess.check_output(cmd))
+
+# Now print them all. I'm using Markdown, which means it's easy to just do `rmet.py|markdown.pl`
+# or if I want to add notes or something I can do it in markdown format at the bottom first
 print("# Micah's progress report as of " + today + "\n")
 print("Tickets I resolved this week:")
 print(resolvedtickets)
@@ -44,3 +71,4 @@ print("Other tickets I worked on this week:")
 print(modifiedtickets)
 print("Tickets I rejected this week:")
 print(rejectedtickets)
+
