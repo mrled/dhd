6519a41e1fa02c218c52dd04f6762c5cdf66d18b
Micah@MICAH-PC <micah@micahrl.com>
2011-11-28 16:21:44 -0600

    sslca.py works now!

--- opt/bin/sslca.py
+++ opt/bin/sslca.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import sys, os, shutil, argparse
+import sys, os, shutil, argparse, logging, subprocess
+#logging.basicConfig(level=logging.CRITICAL) #show only logging.critical() messages
+logging.basicConfig(level=logging.DEBUG) #show all messages up to and including logging.debug() messages
 
 def is_exe(fpath):
     return os.path.exists(fpath) and os.access(fpath, os.X_OK)
 
 def which(program):
-    import os
-
     fpath, fname = os.path.split(program)
     if fpath:
         if is_exe(program):
@@ -17,107 +17,97 @@ def which(program):
             exe_file = os.path.join(path, program)
             if is_exe(exe_file):
                 return exe_file
-
     return None
 
-if (os.name == 'nt'):
-    inpath=which("openssl.exe")
-    if (inpath):
-        opensslbin=inpath
-    else:
-        # check some known locations on Windows
-        for path in [r"C:\Program Files\GnuWin32\bin\openssl.exe",
-                     r"C:\Program Files (x86)\GnuWin32\bin\openssl.exe",
-                     r"C:\Program Files\OpenVPN\bin\openssl.exe",
-                     r"C:\Program Files (x86)\OpenVPN\bin\openssl.exe",
-                     r"C:\git\bin\openssl.exe"]
-            if (is_exe(path)):
-                opensslbin=path
-    # if after all that we have nothing, exit
-    if (!opensslbin):
-        print("Can't find OpenSSL binary. Exiting...")
-        sys.exit(1)
-    # Windows is unlikely to have this variable set, but just in case the user did:
-    if (os.environ['EDITOR']):
-        myeditor=os.environ['EDITOR']
-    else:
-        for path in [r"C:\Program Files\Notepad++\notepad++.exe",
-                     r"C:\Program Files (x86)\Notepad++\notepad++.exe"]
-            if (is_exe(path)):
-                myeditor=path
-    # if after all that we have nothing, use notepad
-    if (!myeditor):
-        myeditor="C:\Windows\system32\notepad.exe"
-
-elif (os.name == 'posix'):
-    inpath=which("openssl")
-    if (inpath):
-        opensslbin=inpath
-    else:
-        print("Can't find OpenSSL binary. Exiting...")
-        sys.exit(1)
-    myeditor=os.environ['EDITOR']
-
 def genprivkey(args):
-    for clientname in args:
+    logging.debug("genprivkey args: %r" % args)
+    for clientname in args.clientname:
         subprocess.check_call([opensslbin, "genrsa", "-out", clientname+".key", "4096"])
-        if (!os.path.exists(clientname+".openssl.cnf")):
+        if not (os.path.exists(clientname+".openssl.cnf")):
+            logging.debug("genprivkey: openssl configuration file not present, copying openssl.cnf.client-DEFAULT to %r" % clientname+".openssl.cnf")
             shutil.copy2("openssl.cnf.client-DEFAULT", clientname+".openssl.cnf")
+            #input("Your editor was opened on the file %r; press return when you have finished editing this file (REQUIRED).")
             subprocess.check_call([myeditor, clientname+".openssl.cnf"])
         subprocess.check_call([opensslbin, "req", "-new", "-nodes", "-config", 
                                clientname+".openssl.cnf", "-key", 
                                clientname+".key", "-out", clientname+".csr"])
 
 def signcerts(args):
-    for clientname in args:
-        subprocess.check_call([opensslbin, c"a", "-batch", "-config",
+    logging.debug("signcerts args: %r" % args)
+    for clientname in args.clientname:
+        subprocess.check_call([opensslbin, "ca", "-batch", "-config",
                                "openssl.cnf.ca", "-in", clientname+".csr",
                                "-out", clientname+".cert", "-days", "7300"])
 
-def gensign:
+def gensign(args):
+    logging.debug("gensign args: %r" % args)
     genprivkey(args)
     signcerts(args)
 
+
 def main(*args):
-    """ORIGINAL HELP: I need to import this more completely into argparse, below. 
-
-sslca: Perform basic tasks for a mini-PKI.
-
-This script can generate private keys and CRLs and sign them with a CA certificate. 
-
-It has several subcommands: 
-
-sslca help              Show this message
-sslca genkey            Generate a private key & CRL for a server
-sslca sign              Sign a CRL with an existing CA key
-sslca gensign           Do both in one step
-
-It expects you to pass it one or more base names, such as "sub.domain.tld", as arguments. For example: 
-
-sslca genkey myserver.tld
-
-It will create files with names based on this, such as myserver.tld.key and myserver.tld.cert. 
-
-Note: the genkey (and also the gensign) subcommand will look for an openssl configuration file named myserver.tld.openssl.cnf. If it does not find one, it will copy openssl.cnf.client-DEFAULT to myserver.tld.openssl.cnf and open your editor on that file. """
+    #logging.debug("main args: " + args)
+    global opensslbin
+    global myeditor
+    if (os.name == 'nt'):
+        inpath=which("openssl.exe")
+        if (inpath):
+            opensslbin=inpath
+        else:
+            # check some known locations on Windows
+            for path in [r"C:\Program Files\GnuWin32\bin\openssl.exe",
+                         r"C:\Program Files (x86)\GnuWin32\bin\openssl.exe",
+                         r"C:\Program Files\OpenVPN\bin\openssl.exe",
+                         r"C:\Program Files (x86)\OpenVPN\bin\openssl.exe",
+                         r"C:\git\bin\openssl.exe"]:
+                if (is_exe(path)):
+                    opensslbin=path
+                    break
+            else:
+                # if after all that we have nothing, exit
+                print("Can't find OpenSSL binary. Exiting...")
+                sys.exit(1)
+        # Windows is unlikely to have this variable set, but just in case the user did:
+        if ('EDITOR' in os.environ):
+            logging.debug("We're on Windows and the %EDITOR% environment variable was set; using that.")
+            myeditor=os.environ['EDITOR']
+        else:
+            for path in [r"C:\Program Files\Notepad++\notepad++.exe",
+                         r"C:\Program Files (x86)\Notepad++\notepad++.exe"]:
+                logging.debug("Checking whether %r is an exe..." % path)
+                if (is_exe(path)):
+                    myeditor=path
+                    logging.debug("Found an editor: %r at this path: %r" % (myeditor, path))
+                    break
+            # if after all that we have nothing, use notepad
+            else:
+                myeditor="C:\Windows\system32\notepad.exe"
+    elif (os.name == 'posix'):
+        # for POSIX systems we're just going to assume that openssl is in the path and $EDITOR is an existing env var. 
+        inpath=which("openssl")
+        if (inpath):
+            opensslbin=inpath
+        else:
+            print("Can't find OpenSSL binary. Exiting...")
+            sys.exit(1)
+        myeditor=os.environ['EDITOR']
 
     argparser = argparse.ArgumentParser(description='Perform basic tasks for a mini-PKI')
     subparsers = argparser.add_subparsers()
     
     subparser_genkey = subparsers.add_parser('genkey', help='Generate a private key & CRL for a server')
-    #TODO: figure out how to add an argument that's required once but optional infinity times
-    #that argument should be here. it's a client name to pass to genkey
-    subparser_genkey.set_defaults(func=genkey)
+    subparser_genkey.add_argument('clientname', nargs='+', action='store', help='Supply a clientname, such as myserver or myserver.sub.domain.tld. The filenames for the cert, CRL, etc are based on this name. This subcommand looks for an openssl configuration file named clientname.openssl.cnf; if it does not find one, it will copy openssl.cnf.client-DEFAULT to clientname.openssl.cnf and open your editor on that file.')
+    subparser_genkey.set_defaults(func=genprivkey)
     
     subparser_sign = subparsers.add_parser('sign', help='Sign a CRL with an existing CA key')
-    #TODO
-    subparser_sign.set_defaults(func=sign)
+    subparser_sign.add_argument('clientname', nargs='+', action='store', help='Supply a clientname, such as myserver or myserver.sub.domain.tld. The filenames for the cert, CRL, etc are based on this name.')
+    subparser_sign.set_defaults(func=signcerts)
 
-    subparser_gensign = subparsers.add_parser('sign', help='Both generate and sign in one step')
-    #TODO
+    subparser_gensign = subparsers.add_parser('gensign', help='Both generate and sign in one step')
+    subparser_gensign.add_argument('clientname', nargs='+', action='store', help='Supply a clientname, such as myserver or myserver.sub.domain.tld. The filenames for the cert, CRL, etc are based on this name. This subcommand looks for an openssl configuration file named clientname.openssl.cnf; if it does not find one, it will copy openssl.cnf.client-DEFAULT to clientname.openssl.cnf and open your editor on that file.')
     subparser_gensign.set_defaults(func=gensign)
     
     parsed = argparser.parse_args()
-    logging.debug("Parsed arguments: %r" % parsed)
     parsed.func(parsed)
 
 if __name__ == '__main__':
