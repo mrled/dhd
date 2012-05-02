05a50dc443cf9dde6e819e6ea6bba1a0fe04af17
Micah R Ledbetter <micah@micahrl.com>
2012-05-01 19:58:29 -0500

    cool new putty functions to make new machines easier. maybe other cleanup too.

--- hbase/Microsoft.PowerShell_profile.ps1
+++ hbase/Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,8 @@
 
 $hostname=[System.Net.Dns]::GetHostName()
 
+$startmenu="$env:appdata\Microsoft\Windows\Start Menu"
+
 
 # aliases can't take parameters (wtf), and functions have different scope than your shell. 
 # Therefore, I can't have a ".b" command like I have to re-source my bash profile.
@@ -187,6 +189,76 @@ function Display-AllCommands {
     }
 }
 set-alias wh display-allcommands
+
+function Create-Shortcut {
+    param(
+        [string]$location
+    )
+    $wshshell = New-Object -ComObject WScript.Shell
+    $lnk = $wshshell.CreateShortcut($location)
+    return $lnk
+}
+
+# a hack but it is a pain to remember how to do this every time ugh. 
+function Initialize-PuttyRsaKey {
+    param ([switch]$NoKeygen)
+ 
+    $sshdir = "$home\.ssh"
+    $ppk = "$sshdir\id_rsa.ppk"
+    $putty_pub = "$sshdir\id_rsa.ppub"
+    $pub = "$sshdir\id_rsa.pub"
+    $privkey = "$sshdir\id_rsa"
+
+    write-host "NOTE: This will only work with RSA keys: `n-$privkey, `n-$putty_pub, `n-$ppk, `n-$pub"
+
+    mkdir -f $sshdir > $null
+    if (-not $NoKeygen.IsPresent) {
+        if ((test-path $ppk) -or (test-path $putty_pub) -or (test-path $ppk) -or (test-path $privkey)) {
+            write-error ("None of these files may exist: $ppk, $putty_pub, $pub, and $privkey .")
+            return
+        }
+        
+        write-host ("Make sure to save your key as $ppk and $putty_pub .")
+        write-host ("Note that you should also export your key as an openssh key to $privkey .")
+        start-process puttygen -wait
+    }
+    if (-not (test-path $ppk) -or -not (test-path $putty_pub) -or -not (test-path $privkey)) {
+        write-error ("You must save your files as $ppk, $putty_pub, and $privkey .")
+        return
+    }
+
+    $pageantlnk_path = "$startmenu\Programs\Startup\pageant.lnk"
+    $pageantlnk = create-shortcut($pageantlnk_path)
+    $pageantlnk.TargetPath = (get-command pageant).Definition
+    $pageantlnk.Arguments = $ppk
+    $pageantlnk.Save()
+    & (gcm $pageantlnk_path).definition
+
+    # by default putty saves pub key files with linebreaks everywhere
+    # $pcontent = get-content $putty_pub
+    # $newcontent = "ssh-rsa "
+    # for ($i=2; $i -lt $pcontent.count -1; $i++) {
+    #     $newcontent += $pcontent[$i]
+    # }
+    # $comment = "$env:username@$hostname-" + $pcontent[1].split("`"")[1]
+    # $newcontent += " $comment"
+    $newcontent = Convert-PuttyRsaPubKey($putty_pub)
+    add-content -path $pub -value $newcontent
+    write-host ("Your pubkey has been saved in openssh format to $pub.")
+}
+
+# By default, putty saves pub key files with linebreaks everywhere. Convert them to openssh format. 
+function Convert-PuttyRsaPubKey {
+    param ([string]$ppbfile)
+    $pcontent = get-content $ppbfile
+    $newcontent = "ssh-rsa "
+    for ($i=2; $i -lt $pcontent.count -1; $i++) {
+        $newcontent += $pcontent[$i]
+    }
+    $comment = "$env:username@$hostname-" + $pcontent[1].split("`"")[1]
+    $newcontent += " $comment"
+    return $newcontent
+}
 
 function head {
     param(
