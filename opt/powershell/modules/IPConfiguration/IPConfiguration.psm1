f52fbe97a1a91663709f58494aa990f440e52de3
Micah R Ledbetter <micah@doublelinepartners.com>
2013-10-18 12:11:14 -0500

    IPConfiguration improvements
    -   get-ipconfiguration is now formatted by default to look
    	like ifconfig
    -   removed lots of duplicated code
    -   minor improvements and bug fixes

--- opt/powershell/modules/IPConfiguration/IPConfiguration.psm1
+++ opt/powershell/modules/IPConfiguration/IPConfiguration.psm1
@@ -1,67 +1,11 @@
-function Get-IPConfiguration2 {
-    [CmdletBinding(DefaultParameterSetName="select")]
-    param(
-        [parameter(ParameterSetName="adapter", Position=0)] [string] $adaptername,
-        [parameter(ParameterSetName="select")]  [switch] [alias("virtual")] $VirtualAdapters,
-        [parameter(ParameterSetName="select")]  [switch] [alias("disconnected")] $DisconnectedAdapters,
-        [parameter(ParameterSetName="select")]  [switch] [alias("tunnels")] $TunnelAdapters,
-        [parameter(ParameterSetName="select")]  [switch] [alias("loopback")] $LoopbackAdapters,
-        [parameter(ParameterSetName="all")]     [switch] [alias("all")] $allAdapters,
-        [parameter(ParameterSetName="global")]  [switch] $globalProperties
-    )
-    $reqDotNetNics = @()
-
-    $paramset = $PSCmdlet.ParameterSetName.tolower()
-    if ($paramset -eq "global") {
-        $gloprops = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPglobalProperties()
-        $reqDotNetNics = $gloprops
-    }
-    elseif ($paramset -eq "all") {
-        $reqDotNetNics = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
-    }
-    elseif ($paramset -eq "adapter") {
-        $allnics = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
-        foreach ($n in $allnics) {
-            if ($n.name -eq $adaptername) {
-                $reqDotNetNics += $n
-            }
-            if ($reqDotNetNics.length -eq 0) {
-                throw "Requested a NIC called $adaptername, but no such NIC exists"
-            }
-        }
-    }
-    elseif ($paramset -eq "select") {
-        $allnics = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
-        foreach ($n in $allnics) {
-            $desc = $n.Description
-            if ($n.OperationStatus -eq "Down") {
-                if ($DisconnectedAdapters.ispresent) {
-                    $reqDotNetNics += @($n)
-                }
-            }
-            elseif ($desc -and ($desc.contains("VMware Virtual Ethernet Adapter") -or $desc.contains("VirtualBox Host-Only Ethernet Adapter"))) {
-                if ($VirtualAdapters.ispresent) {
-                    $reqDotNetNics += @($n)
-                }
-            }
-            elseif (([string]$n.NetworkInterfaceType).tolower() -eq "tunnel") {
-                if ($TunnelAdapters.ispresent) {
-                    $reqDotNetNics += @($n)
-                }
-            }
-            elseif (([string]$n.NetworkInterfaceType).tolower() -eq "loopback") {
-                if ($LoopbackAdapters.ispresent) {
-                    $reqDotNetNics += @($n)
-                }
-            }
-            else {
-                $reqDotNetNics += @($n)
-            }
-        }
-    }
-    return $reqDotNetNics
-}
-
+# todo: 
+# - show more ipv6 info
+# - show dhcp 
+# - show whether ip routing / wins gateway is enabled in the global section like ipconfig
+# - figure out the right way to include global IP information in this (probably a separate command honestly)
+# - show the number of adapters that are hidden - ?? probably not
+# - create a class for IPAddress objects and use that here
+# - create views in the format.ps1xml file for tables and lists as well
 function Get-IPConfiguration {
     [CmdletBinding(DefaultParameterSetName="select")]
     param(
@@ -130,7 +74,15 @@ function Get-IPConfiguration {
         $ip = @()
         foreach ($addr in $props.UnicastAddresses) {
             #$ip += @(@{ IPAddress = $addr.Address.IPAddressToString; CIDR = $addr.PrefixLength; })
-            $ip += @("$($addr.Address.IPAddressToString)/$($addr.PrefixLength)")
+            #$ip += @("$($addr.Address.IPAddressToString)/$($addr.PrefixLength)")
+            $ip += $addr.Address.IPAddressToString
+        }
+        $mac = $nic.GetPhysicalAddress()
+        if ($mac.tostring().length -gt 0) { 
+            $mac = Convert-HexStringToMac $mac
+        }
+        else {
+            $mac = "NONE"
         }
         $property = @{
             Name = $nic.name
@@ -139,15 +91,18 @@ function Get-IPConfiguration {
             IPv4Mask = $props.UnicastAddresses.IPv4Mask.IPAddressToString
             DefaultGateway = $props.GatewayAddresses.Address.IPAddressToString
             DNSServer = $props.DnsAddresses.IPAddressToString
-            MAC = $nic.GetPhysicalAddress()
+            MAC = $mac
+            NetworkInterfaceType = $nic.NetworkInterfaceType
             DotNetObject = $nic
         }
         $nicobj = new-object PSObject -property $property
-        $nicobj.PSObject.Typenames.insert(0,'IPConfigurationNeworkAdapter')
+        $nicobj.PSObject.Typenames.insert(0,'IPConfigurationNetworkAdapter')
         $reqNetworkAdapters += @($nicobj)
     }
     return $reqNetworkAdapters
 }
+set-alias ifconfig Get-IPConfiguration
+set-alias getip Get-IPConfiguration
 
 function Convert-HexStringToMac {
     param([parameter(mandatory=$true)] [string] $hex)
@@ -174,11 +129,6 @@ function Get-Listeners2 {
     $listeners
 }
 
-# This works pretty well right now, but it has a couple of areas for improvement:
-# - calling format-table at the end means I can't sort-object with the results of this command. 
-#   is there a way to not call format-table if I'm going to pipe the results?
-# - if I'm going to display the content, then this is correct. However, if I'm going to pipe
-#   the content, it'd be better to spit out the results as I receive them. 
 function Get-Listeners {
     $listeners = @()
     foreach ($line in (netstat -ano)) {
@@ -209,9 +159,7 @@ function Get-Listeners {
             $listeners += @($l)
         }
     }
-    $ftProps = @('Address','Port','Protocol','PID','ProcessName')
-    $soProps = @('AddressFamily','Port')
-    $listeners | sort-object -property $soProps #| format-table -property $ftProps 
+    $listeners | sort-object -property 'AddressFamily','Port'
 }
 set-alias listens get-listeners
 set-alias listeners get-listeners
@@ -221,7 +169,7 @@ function resolve-hostname {
         [parameter(mandatory=$true)] [string[]] $domainName
     )
     foreach ($dn in $domainName) {
-        write-host "$dn\:"
+        write-host "$dn`:"
         foreach ($a in [System.Net.Dns]::Resolve($dn).AddressList) {
             write-host "    $a"
         }
@@ -230,53 +178,11 @@ function resolve-hostname {
 }
 set-alias resolve resolve-hostname
 
-
 function Get-PublicIPAddress {
     invoke-restmethod icanhazip.com
 }
 set-alias icanhazip Get-PublicIPAddress
 set-alias canhazip Get-PublicIPAddress
-
-# todo: 
-# - show more ipv6 info
-# - show dhcp 
-# - show whether ip routing / wins gateway is enabled in the global section like ipconfig
-# - throw an error if you pass -global to this function 
-# - show the number of adapters that are hidden
-# I think the thing to do is make Powershell objects for each one with all the data *I* want
-# and then add the .NET object in there as _DotNetAdapter or something
-# And then make it so Get-IPConfiguration displays data as nicely as ifconfig does now
-# ... oh, looks like I can do that with `format-table -property`!
-function Show-IPConfiguration {
-    $global = Get-IPConfiguration2 -global
-    write-output "Hostname: $($global.HostName).$($global.DomainName)`n"
-    #write-output "    Public IP: $(Get-PublicIPAddress)"
-
-    $requestedconfigs = Get-IPConfiguration2 @args
-    foreach ($adapter in $requestedconfigs) {
-        $props = $adapter.GetIPProperties()
-        write-output $adapter.name
-        write-output "    $($adapter.Description)"
-        foreach ($addr in $props.UnicastAddresses) {
-            write-output "    IP Address: $($addr.Address.IPAddressToString)/$($addr.PrefixLength)"
-        }
-        write-output "    Default Gateway: $($props.GatewayAddresses.Address.IPAddressToString)"
-        write-output "    DNS Servers: $($props.DnsAddresses.IPAddressToString)"
-        $mac = $adapter.GetPhysicalAddress()
-        if ($mac.tostring().length -gt 0) { 
-            $mac = Convert-HexStringToMac $mac
-        }
-        else {
-            $mac = "NONE"
-        }
-        write-output "    Link type: $($adapter.NetworkInterfaceType), MAC: $mac"
-        write-output ""
-    }
-}
-
-set-alias ifconfig Show-IPConfiguration
-set-alias getip Get-IPConfiguration
-
 
 $exFunction = @(
     'Get-IPConfiguration'
