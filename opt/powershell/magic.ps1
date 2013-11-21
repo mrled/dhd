3d043aaa5be7f4c7f9813d35ec57c4d687df3d55
Micah R Ledbetter <micah@doublelinepartners.com>
2013-11-21 11:54:21 -0600

    Added Powershell Magic Pulldown Script Of Bullshit Or Whatever Dot Ps1

--- opt/powershell/magic.ps1
+++ opt/powershell/magic.ps1
@@ -1,4 +1,3 @@
-$erroractionpreference = "stop"
 
 function invoke-magic {
 
@@ -11,16 +10,18 @@ function invoke-magic {
     if ([Environment]::Is64BitProcess) {
         start-job { Set-ExecutionPolicy unrestricted -scope $sepscope -force } -RunAs32
     }
+    $magicJobs = @()
 
-    $dhdClonedEv = Register-EngineEvent -SourceIdentifier DhdCloned -Action {
+    $erroractionpreference = "silentlycontinue"
+    $magicJobs += @(Register-EngineEvent -SourceIdentifier DhdCloned -Action {
         copy ~/.dhd/opt/win32/ConEmu.xml $env:appdata
         mkdir ~/Documents/WindowsPowerShell -force | out-null
         $dhdprofile = "~/.dhd/hbase/Microsoft.Powershell_profile.win32.ps1"
         copy $dhdprofile ~/Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1
         . $dhdprofile
         reinit # from my initialization.ps1 file in .dhd
-    }
-    $gitInstalledEv = Register-EngineEvent -SourceIdentifier GitInstalled -Action {
+    })
+    $magicJobs += @(Register-EngineEvent -SourceIdentifier GitInstalled -Action {
         Register-EngineEvent -SourceIdentifier DhdCloned -Forward
         foreach ($maybegit in "C:\Program Files (x86)\git\cmd\git.exe","C:\Program Files\git\cmd\git.exe") {
             if (test-path $maybegit) {
@@ -30,15 +31,20 @@ function invoke-magic {
         if (-not (test-path alias:/git)) {
             write-error "Can't find git - where was it installed?"
         }
+        if (test-path $home/.dhd) {
+            rm -force -recurse $home/.dhd
+        }
         git clone https://github.com/mrled/dhd.git $home/.dhd
         $null = New-Event -SourceIdentifier DhdCloned
-    }
-    $psgetInstalledEv = Register-EngineEvent -SourceIdentifier PsgetInstalled -Action {
-        import-module psget
-        install-module PSReadLine,TabExpansion++
-    }
+    })
+    $magicJobs += @(Register-EngineEvent -SourceIdentifier PsgetInstalled -Action {
+        import-module "C:\Program Files\Common Files\Modules\PsGet\PsGet.psm1"
+        install-module "PSReadLine"
+        install-module "TabExpansion++"
+        install-module "PSCX"
+    })
 
-    $chocolateyInstalledEv = Register-EngineEvent -SourceIdentifier ChocolateyInstalled -Action { 
+    $magicJobs += @(Register-EngineEvent -SourceIdentifier ChocolateyInstalled -Action { 
         Register-EngineEvent -SourceIdentifier GitInstalled -Forward
         Register-EngineEvent -SourceIdentifier PsgetInstalled -Forward
         Register-EngineEvent -SourceIdentifier ConemuInstalled -Forward
@@ -48,9 +54,9 @@ function invoke-magic {
         $null = New-Event -SourceIdentifier PsgetInstalled
         cinst conemu
         $null = New-Event -SourceIdentifier ConemuInstalled
-    }
+    })
 
-    $ChocolateyJob = start-job -name InstallChocolatey -scriptblock {
+    $magicJobs += @(start-job -name InstallChocolatey -scriptblock {
         Register-EngineEvent -SourceIdentifier ChocolateyInstalled -Forward
         iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
         [Environment]::SetEnvironmentVariable("path", $env:path+";${env:systemdrive}\chocolatey\bin", "User")
@@ -58,10 +64,74 @@ function invoke-magic {
             [Environment]::SetEnvironmentVariable("path", $env:path+";${env:systemdrive}\chocolatey\bin", "User")
         }
         $null = New-Event -SourceIdentifier ChocolateyInstalled
-    }
-
-    return $ChocolateyJob
+    })
+    $erroractionpreference = "stop"
+    return $magicJobs
 }
 
+function Show-StatusLoop {
+    param(
+        [parameter(mandatory=$true)] $jobs
+    )
+    write-host "Casting magical spells and whatever bullshit..."
+    $complete = $false
+    $counter = 0
+    while (-not $complete) {
+        $anyFailed = $anyUnstarted = $false
+        foreach ($job in $jobs) {
+            switch ($job.state) {
+                Failed {
+                    $anyFailed = $true
+                    $stateColor = "red"
+                }
+                NotStarted {
+                    $anyUnstarted = $true
+                    $stateColor = "yellow"
+                }
+                Running {
+                    $stateColor = "blue"
+                }
+                Completed {
+                    $stateColor = "green"
+                }
+                default {
+                    $stateColor = "white"
+                }
+            }
+            write-host -nonewline "$counter " -foreground magenta
+            write-host -nonewline $job.name
+            write-host -nonewline ": "
+            write-host $job.state -foreground $stateColor
+        }
+        if ($anyFailed -or (-not $anyUnstarted)) { $complete = $true }
+        $counter += 1
+        sleep 2
+    }
+    write-host "========"
+    foreach ($job in $jobs) {
+        write-host "Job: $($job.name): " -nonewline 
+        write-host $job.state
+        if ($job.output) {
+            write-host "$($job.name) Output: " 
+            write-host $job.output -foreground green
+        }
+        if ($job.warning) {
+            write-host "$($job.name) Warning: "
+            write-host $job.warning -foreground yellow
+        }
+        if ($job.error) {
+            write-host "$($job.name) Error: "
+            write-host $job.error -foreground red
+        }
+    }
+    write-host "========"
+    if ($anyFailed) {
+        write-host "~*~ SPELL CASTING WAS UNSUCCESSFUL ~*~" -foreground red
+    }
+    else {
+        write-host "~*~ SPELL CASTING WAS SUCCESSFUL ~*~" -foreground green
+    }
+}
 
-# get console and settings from dropbox
+# $magicJobs = invoke-magic 
+# Show-StatusLoop $magicJobs
