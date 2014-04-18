0cda8a105cef48ce5ce206c8f4f7fb1dd53011f8
Micah R Ledbetter <micah@doublelinepartners.com>
2014-04-18 18:58:45 -0500

    Make the prompt prettier again

--- opt/powershell/SolutionScripts.profile.ps1
+++ opt/powershell/SolutionScripts.profile.ps1
@@ -1,6 +1,10 @@
 ﻿$promptSuffix = {
-    Write-Host "SS" -nonewline -foregroundcolor White
-    Write-Host "ϫ" -nonewline -foregroundcolor Magenta
+    #Write-Host "SS" -nonewline -foregroundcolor White
+    #Write-host "$([char]1003)" -nonewline -foregroundcolor Magenta
+    # Find something that looks sort of like the visual studio symbol...
+    # [char]42479   ꗯ (VAI SYLLABLE GBE)
+    # [char]1003    ϫ (COPTIC SMALL LETTER GANGIA)
+    write-host " $([char]42479) " -nonewline -foregroundcolor White -backgroundcolor Magenta
 }
 
 $profile | Add-Member -MemberType NoteProperty -Name "SolutionScripts" -Value $myinvocation.mycommand.path -force
