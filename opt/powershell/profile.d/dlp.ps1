adc8e0d3f0a0bf4ab127ea1ee225c624bdf6c517
Micah R Ledbetter <micah@doublelinepartners.com>
2014-10-09 12:40:54 -0500

    Powershell: work-related fixes and improvements
    - Better DLP client tracking system
    - Removed old functions that were no longer useful
    - Moved to a profile.d thing

---
+++ opt/powershell/profile.d/dlp.ps1
@@ -0,0 +1,391 @@
+<#
+ASSUMPTIONS: 
+- $DLPProjectBase must be set to an existing directory (this is done in umodules/dlp.ps1)
+- You must have micah's DLPLogisticsModules checked out to $DLPProjectBase/misc/DLPLogisticsModules
+#>
+
+if (-not ($VisualStudioDirectories -contains $DLPProjectBase)) {
+    $VisualStudioDirectories += @($DLPProjectBase)
+}
+
+<#
+function New-DlpProject {
+    param(
+        [parameter(mandatory=$true)] [string] $LocalName,
+        [parameter(mandatory=$true)] [string] $GitHubOrg,
+        [switch] $Checkout,
+        [string[]] $Repositories
+    )
+    $project = New-Object PSObject
+    Add-Member -InputObject $project -NotePropertyMembers  @{
+        Repositories = $Repositories
+        Checkout = $Checkout
+        LocalName = $LocalName
+        GitHubOrg = $GitHubOrg
+    }
+    Add-Member -InputObject $project -MemberType ScriptProperty -Name Directory -Value {
+        $projDir = "$DLPProjectBase/$($this.Localname)"
+        if (-not $this.checkout) {
+            return ""
+        }
+        elseif (test-path $projDir) {
+            return get-item $projDir
+        }
+        else {
+            return "$projDir"
+        }
+    }
+    return $project
+}
+#>
+
+function New-DlpProject {
+    param(
+        [parameter(mandatory=$true)] [string] $GitHubOrg,
+        [string] $LocalName,
+        [switch] $Checkout,
+        [string[]] $Repositories,
+        [hashtable] $Contexts
+    )
+    $project = New-Object PSObject
+    Add-Member -InputObject $project -NotePropertyMembers  @{
+        _ExplicitRepos = $Repositories
+        _ExplicitCheckout = $Checkout
+        LocalName = if ($LocalName) { $Localname } else { $GitHubOrg }
+        GitHubOrg = $GitHubOrg
+        Contexts = $Contexts
+    }
+    # $project.Checkout should be true if -Checkout is passed or if there are any -Contexts specified
+    Add-Member -InputObject $project -MemberType ScriptProperty -Name Checkout -Value {
+        return $this._ExplicitCheckout -or [bool]$this.Contexts.count
+    }
+    # $project.Repositories contains all items passed to -Repositories and all repos set in values of the -Contexts hashtable
+    Add-Member -InputObject $project -MemberType ScriptProperty -Name Repositories -Value {
+        $repos = @()
+        $repos += $this._ExplicitRepos
+        foreach ($ctx in $this.Contexts.keys) {
+            foreach ($repo in $this.Contexts[$ctx]) {
+                $repos += $repo
+            }
+        }
+        $repos | sort -uniq
+    }
+    Add-Member -InputObject $project -MemberType ScriptProperty -Name Directory -Value {
+        $projDir = "$DLPProjectBase/$($this.Localname)"
+        if (-not $this.checkout) {
+            return ""
+        }
+        elseif (test-path $projDir) {
+            return get-item $projDir
+        }
+        else {
+            return "$projDir"
+        }
+    }
+    return $project
+}
+
+ipmo "$DLPProjectBase\misc\DLPLogisticsModules\encrypted-credentials"
+
+<#
+$_DLPOrganizationList = @(
+    New-DlpProject -LocalName mrled -GitHubOrg mrled -Checkout -Repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Tools','Ed-Fi-Dashboards-Core','Ed-Fi-ODS','Ed-Fi-ODS-Implementation','Ed-Fi-Common','minipki') 
+    New-DlpProject -LocalName dlp -GitHubOrg DoubleLinePartners -Checkout -Repositories @('InternalTools')
+    New-DlpProject -LocalName alliance -GitHubOrg 'Ed-Fi-Alliance' -Checkout -Repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Dashboards-Core','Ed-Fi-ODS','Ed-Fi-ODS-Implementation','Ed-Fi-Common') 
+    New-DlpProject -LocalName msdf -GitHubOrg 'DoubleLinePartners-MSDF' -repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Common') 
+    New-DlpProject -LocalName tdoe -GitHubOrg TennesseeDOE -checkout -repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Dashboards-Core','Ed-Fi-ODS','Ed-Fi-Common') 
+    New-DlpProject -LocalName nedoe -GitHubOrg NebraskaDOE -checkout -repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Dashboards-Core','Ed-Fi-ODS','Ed-Fi-Common') 
+    New-DlpProject -LocalName pde -GitHubOrg PennsylvaniaDOE -checkout -repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Common') 
+)
+#>
+$DLPOrganizations = @{}
+@(
+    New-DlpProject -LocalName mrled -GitHubOrg mrled -Checkout `
+        -Repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Tools','Ed-Fi-Dashboards-Core','Ed-Fi-ODS','Ed-Fi-ODS-Implementation','Ed-Fi-Common','minipki') 
+    New-DlpProject -LocalName dlp -GitHubOrg DoubleLinePartners -Checkout -Repositories @('InternalTools')
+    New-DlpProject -LocalName alliance -GitHubOrg 'Ed-Fi-Alliance' -Checkout `
+        -Contexts @{ rest = @('Ed-Fi-Common','Ed-Fi-ODS','Ed-Fi-ODS-Implementation') } `
+        -Repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Dashboards-Core') 
+    New-DlpProject -LocalName msdf -GitHubOrg 'DoubleLinePartners-MSDF' -repositories @('Ed-Fi-Apps','Ed-Fi-Core','Ed-Fi-Common') 
+    New-DlpProject -LocalName edfiteam -GitHubOrg 'DoubleLinePartners-MSDF' -Contexts @{
+        dash = @('Ed-Fi-Core','Ed-Fi-Apps') 
+    }
+    New-DlpProject -LocalName tdoe -GitHubOrg TennesseeDOE `
+        -Contexts @{ 
+            rest = @('Ed-Fi-Common','Ed-Fi-ODS') 
+            dash = @('Ed-Fi-Common','Ed-Fi-Dashboards-Core','Ed-Fi-Apps')
+        }
+    New-DlpProject -LocalName nedoe -GitHubOrg NebraskaDOE `
+        -Contexts @{ 
+            rest = @('Ed-Fi-Common','Ed-Fi-ODS') 
+            dash = @('Ed-Fi-Common','Ed-Fi-Core','Ed-Fi-Apps')
+        }
+    New-DlpProject -LocalName pde -GitHubOrg PennsylvaniaDOE -Contexts @{ dash = @('Ed-Fi-Common','Ed-Fi-Core','Ed-Fi-Apps') } `
+) |% { $DLPOrganizations[$_.LocalName] = $_ }
+
+function Update-DlpGitProject {
+    [CmdletBinding()]
+    param(
+        [array] $clientList
+    )
+
+    $workingClientList = @()
+    foreach ($client in $clientList) {
+        $clientObject = $DLPOrganizations.$client
+        if (-not $clientObject) {
+            throw "Passed client '$client', but no such client exists in `$DLPOrganizations"
+        }
+        $workingClientList += @($clientObject)
+    }
+    if (-not $clientList) {
+        $workingClientList = $DLPOrganizations.values
+    }
+
+    foreach ($client in $workingClientList) {
+        if (-not $client.Checkout) {
+            write-verbose "Client '$($client.LocalName)' was not set to be checked out."
+        }
+        else {
+            write-verbose "Updating client '$($client.LocalName)'"
+            $projectDir = "$DLPProjectBase\$($client.LocalName)"
+            if (-not (test-path $projectDir)) {
+                mkdir $projectDir | out-null
+            }
+            foreach ($repoName in $client.Repositories) {
+                set-location $projectDir 
+                # $repoClients is all the organizations that contain $repoName
+                $repoClients = ($DLPOrganizations.values |? { $_.Repositories -contains $repoName }).LocalName
+                if (-not (test-path "$projectDir\$repoName")) {
+                    write-verbose "  Doing initial clone for '$repoName' for project '$($client.LocalName)'"
+                    git clone --origin "$($client.LocalName)" "git@github.com:$($client.GitHubOrg)/$repoName"
+                }
+                else {
+                    write-verbose "  Updating repository '$repoName' for project '$($client.LocalName)'"
+                }
+                set-location $projectDir\$repoName
+                foreach ($gitRemoteName in $repoClients) {
+                    if ((git remote) -notcontains $gitRemoteName) {
+                        write-verbose "    Adding '$gitRemoteName' remote for '$repoName' repository"
+                        $ghoName = ($DLPOrganizations.values |? { $_.LocalName -match $gitRemoteName }).GitHubOrg
+                        git remote add $gitRemoteName "git@github.com:$ghoName/$repoName"
+                    }
+                    else {
+                        write-verbose "    Found '$gitRemoteName' remote for '$repoName' repository"
+                    }
+                }
+                write-verbose "  Doing 'git fetch' in repository '$repoName' for project '$($client.LocalName)'"
+                git fetch --all
+            }
+        }
+    }
+}
+
+<#
+Functionality I'd like to have: 
+- Search across all checked out repositories (would be dependent on current branch checked out)
+- Search across all repos for one client, or by individual client contexts
+- Ability to use 'ack' for faster search (via a function or switch)
+- Check out default branch named after client (so tdoe-development rather than just development)
+- Change into various project contexts, which reimports path-resolver for your project
+- Better doxx for these functions
+- Fewer typings? E.g. this is really long: cd "$($DLPOrganizations.tdoe.Directory)/Ed-Fi-ODS/logistics" 
+#>
+
+<#
+$projPaths = @("D:\Projects","C:\Projects") #This can be an array. 
+
+$edfiBasePath = "C:\Projects\DLP"
+$edfiCorePath = "C:\Projects\DLP\Ed-Fi-Core"
+$edfiAppsPath = "C:\Projects\DLP\Ed-Fi-Apps"
+$edfiToolsPath = "C:\Projects\DLP\Ed-Fi-Tools"
+
+
+function Get-DlpProjectBasePath {
+    param(
+        [validateset("core","apps","all","current")] [string] $repo = "all",
+        [string] $basePath = $null
+    )
+    if (-not $basePath) {
+        $curpath = (get-item $pwd).fullname 
+        foreach ($pp in @($projPaths)) {
+            if ($curpath.startswith($pp)) {
+                $splitPath = $curpath -split '\\'
+                $basePath = $splitpath[0..2] -join '\' #this will be something like C:\Projects\DLP
+                $currentRepo = $splitpath[3] # this will be something like Ed-Fi-Apps or whatever
+                break
+            }
+        }
+    }
+    if (-not $basePath) {
+        $basePath = $edfiBasePath
+    }
+    $appsPath = "$basePath\Ed-Fi-Apps"
+    $corePath = "$basePath\Ed-Fi-Core"
+    switch ($repo) {
+        "core" { $location = "$corePath" }
+        "apps" { $location = "$appsPath" }
+        "all" { $location = "$corePath","$appsPath" }
+        "current" { $location = "$basePath\$currentRepo"}
+    }
+    return get-item $location
+}
+
+function Get-DlpProjectFile {
+    [CmdletBinding(DefaultParametersetName="QueryType")] 
+    param(
+        [Parameter(ParameterSetName='Query',Position=0)] [alias("query")] [string[]] $include,
+        [Parameter(ParameterSetName='Query')] [string[]] $exclude,
+        [Parameter(ParameterSetName='Query')] [string[]] $subdir,
+
+        [Parameter(ParameterSetName='QueryType')] 
+        [validateset('logistics','source','visualstudio','database','etl','alltypes','allfiles')] 
+        [string[]] $type = 'logistics',
+
+        [string] $containingPattern,
+        [switch] $caseSensitive,
+
+        [validateset("core","apps","all","current")] [string] $repo = "all",
+        [string] $basePath = $null
+    )
+    $searchBase = Get-DlpProjectBasePath -repo $repo -basePath $basePath
+
+    # A bunch of predefined query types, to match the -type parameter. Just maakes things convenient.
+    $qtypes = @{}
+    $qtypes.logistics = @{}
+    $qtypes.logistics.include = @('*.ps1','*.psm1','*.psd1','credentials-*.xml')
+    $qtypes.logistics.subdir = @('logistics','src\SolutionScripts')
+    $qtypes.source = @{}
+    $qtypes.source.include = @('*.cs')
+    $qtypes.source.subdir = @('Application','src')
+    $qtypes.visualstudio = @{}
+    $qtypes.visualstudio.include = @('*.sln','*.csproj','*.ccproj','*.config','*.cscfg','*.cspkg','*.csdef')
+    $qtypes.visualstudio.subdir = @('Application','src')
+    $qtypes.config = @{}
+    $qtypes.config.include = @('*.config')
+    $qtypes.config.subdir = @('Application','src')
+    $qtypes.database = @{}
+    $qtypes.database.include = @('*.sql')
+    $qtypes.database.subdir = @('Database')
+    $qtypes.etl = @{}
+    $qtypes.etl.include = @('*.dtsx')
+    $qtypes.etl.subdir = @('Etl')
+    $qtypes.alltypes = @{}
+    $qtypes.alltypes.include = foreach ($k in $qtypes.keys) { $qtypes[$k]['include'] } 
+    $qtypes.alltypes.subdir = foreach ($k in $qtypes.keys) { $qtypes[$k]['subdir'] }
+    $qtypes.allfiles = @{}
+    $qtypes.allfiles.include = ''
+    $qtypes.allfiles.subdir = '.'
+
+    if ($PsCmdlet.ParameterSetName -eq 'QueryType') {
+        $include = $qtypes["$type"]['include']
+        $subdir = $qtypes["$type"]['subdir'] 
+    }
+
+    $expandedLocations = @()
+    foreach ($sd in @($subdir)) {
+        foreach ($sb in @($searchBase)) {
+            if (test-path "$sb\$sd") {
+                $expandedLocations += "$sb\$sd"
+            }
+        }
+    }
+
+    $verbMessage = "Looking in '$expandedLocations' for files like '$include'"
+    if ($exclude) { $verbMessage += " but not like '$exclude'"}
+    write-verbose $verbMessage
+    $allMatches = gci -recurse $expandedLocations -include $include -exclude $exclude
+    if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) { 
+        write-verbose "Matches:"
+        foreach ($match in $allmatches) {
+            write-verbose $match.name
+        }
+    }
+    if ($containingPattern) {
+        $slsParms = @{
+            quiet = $true
+            pattern = $containingPattern
+            CaseSensitive = $caseSensitive.ispresent
+        }
+        write-verbose "Searching through results files for strings matching '$containingPattern'..."
+        $results = $allMatches |? { $_ | sls @slsparms }
+    }
+    else {
+        $results = $allMatches
+    }
+    return $results
+}
+set-alias gdlp Get-DlpProjectFile
+
+function Set-DLPProjectContext {
+    param(
+        [parameter(mandatory=$true)] [ValidateSet("rest","dash","old")] $context
+    )
+    switch ($context) {
+        "rest" {
+            $env:PathResolverRepositoryOverride = "TDOE-RestApiSpike"
+        }
+        "dash" {
+            $env:PathResolverRepositoryOverride = "Ed-Fi-Common;Ed-Fi-Dashboards-Core;Ed-Fi-Apps"
+        }
+        "old" {
+            $env:PathResolverRepositoryOverride = "Ed-Fi-Core;Ed-Fi-Apps"
+        }
+    }
+    $LoadedPathResolver = get-module | where { $_.Name -eq "path-resolver" }
+    if ($LoadedPathResolver) {
+        remove-module path-resolver
+    }
+    ipmo "$edfiBasePath\TDOE-RestApiSpike\logistics\scripts\modules\path-resolver.psm1"
+}
+set-alias setdlp Set-DLPProjectContext
+#>
+
+function Generate-DlpCredentialCertificate {
+    param(
+        [parameter(mandatory=$true)] [string] $certName,
+        [parameter(mandatory=$true)] [string] $pfxPassword,
+        [int] $keySize = 4096,
+        [int] $daysValid = 7300
+    )
+    Invoke-OpenSsl @( 'req', '-x509', '-nodes', '-days', "$daysValid", "-subj", "`"/CN=$certName`"", 
+        "-newkey", "rsa:$keysize", "-keyout", "`"$certName.pem`"", "-out", "`"$certName.pem`"")
+    Convert-OpenSSLPemToPfx -pemfile "$certName.pem" -pfxPassword $pfxPassword
+}
+
+function Invoke-GitCommandOnMultipleRepositories {
+    param(
+        [Parameter(mandatory=$true)] [string] $gitCommand,
+        [Parameter(mandatory=$true)] [array] $repositoryList
+    )
+    $resolvedRepositoryList = @()
+    foreach ($repo in $repositoryList) {
+        $resolvedRepositoryList += @(resolve-path $repo)
+    }
+
+    $oldpath = get-location
+    foreach ($repoLocation in $resolvedRepositoryList) {
+        set-location $repoLocation | out-null
+        write-host -foreground magenta $repoLocation
+        invoke-expression "git $gitCommand"
+    }
+    set-location $oldpath
+}
+set-alias mgit Invoke-GitCommandOnMultipleRepositories
+
+function Connect-DLPFlameleafVPN {
+    $DomainCreds = Get-DLPProtectedCredential -credentialFile ~/credentials-MRLDLP.xml -credentialName "micah@doubleline.us"
+    ipsecc -a -r FlameLeafVPN.vpn -u $DomainCreds.Username -p "$($DomainCreds.DecryptPassword())"
+}
+
+function Delete-TeamCityBuild {
+    param(
+        $teamcityhost, 
+        $build, 
+        $username, 
+        $password
+    )
+    $encodedcredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($username+":"+$password))
+    $buildUri = "$teamcityhost/httpAuth/app/rest/builds/id:$build"
+    $headers = @{"Authorization"="Basic $encodedcredentials"}
+    Invoke-WebRequest -Uri $buildUri -Method Delete -Headers $headers
+}
