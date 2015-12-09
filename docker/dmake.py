84102e3e49c79007dc7d3118ae3dabe5e6eff0f1
Micah R Ledbetter <mrled@younix.us>
2015-12-08 20:50:29 -0600

    Progress further on TShock Docker image
    - marionettist general cleanup
    - fix Dockerfile templating
    - dmake can now build and push Docker images

--- docker/dmake.py
+++ docker/dmake.py
@@ -3,9 +3,11 @@
 import argparse
 import os
 import shutil
-#import subprocess
+import string
+import subprocess
 import sys
 
+docker_hub_user="mrled"
 script_root = os.path.dirname(os.path.realpath(__file__))
 marionettist_common_template = """
 ### START Marionettist Docker configuration
@@ -16,7 +18,7 @@ RUN \
     apt-get -y install \
         python \
         ansible
-{insert_files}
+$insert_files
 RUN ansible-playbook /marionettist/marionettist-docker-container.yml --connection=local
 
 ### END Marionettist Docker configuration
@@ -29,6 +31,15 @@ marionettist_common_files = {
     '../ansible/marionettist-docker-container.yml': 'marionettist/marionettist-docker-container.yml',
     '../ansible/inventory/marionettist-docker-container': 'etc/ansible/hosts'
 }
+
+class AsciiEscapes:
+    Blue = '\033[94m'
+    Green = '\033[92m'
+    Yellow = '\033[93m'
+    Red = '\033[91m'
+    Default = '\033[0m'
+    Bold = '\033[1m'
+    Underline = '\033[4m'
 
 # Just fucking make a directory
 # If you have to make a whole tree, just fucking do it
@@ -56,44 +67,111 @@ def copy_common_files(build_dir):
         just_fucking_copy(src, dst)
 
 def insert_common_dockerfile(build_dir): 
+    print('{}Creating Dockerfile...{}'.format(AsciiEscapes.Green, AsciiEscapes.Default))
     in_df_path  = os.path.abspath("{}/Dockerfile.template".format(build_dir))
     out_df_path = os.path.abspath("{}/Dockerfile".format(build_dir))
     with open(in_df_path) as in_file:
-        in_df_contents = in_file.read()
+        df_template = string.Template(in_file.read())
 
     add_stanza = ""
     for file_entry in marionettist_common_files.values():
         add_stanza += "ADD {0} /{0}\n".format(file_entry)
-    marionettist_common_dockerfile = marionettist_common_template.format(**{'insert_files': add_stanza})
-    out_df_contents  = in_df_contents.format(**{'marionettist_common': marionettist_common_dockerfile})
+    
+    common_mapping = {
+        'insert_files': add_stanza
+    }
+    common_template = string.Template(marionettist_common_template)
+    marionettist_common_dockerfile = common_template.safe_substitute(**common_mapping)
+
+    template_mapping = {
+        'marionettist_common': marionettist_common_dockerfile
+    }
+
+    out_df_contents = df_template.safe_substitute(**template_mapping)
 
     with open(out_df_path, 'w') as out_file:
         print(out_df_contents, file=out_file)
 
-def build_docker_dir(dockername, build_dir):
+    print(out_df_contents)
+
+def populate_build_dir(imagename, build_dir):
+    print('{}Creating build directory at "{}"...{}'.format(
+            AsciiEscapes.Green, build_dir, AsciiEscapes.Default))
     if os.path.exists(build_dir):
         shutil.rmtree(build_dir)
-    base_df_path = os.path.abspath("{}/{}/Dockerfile.template".format(script_root, dockername))
+    base_df_path = os.path.abspath("{}/{}/Dockerfile.template".format(script_root, imagename))
     just_fucking_makedirs(build_dir)
     shutil.copy(base_df_path, build_dir)
     insert_common_dockerfile(build_dir)
     copy_common_files(build_dir)
-    print("Build directory created at {}".format(build_dir))
 
-#def run_docker(build_dir):
+def docker_build(imagename, imagetag, build_dir): 
+    print('{}Building docker image...{}'.format(
+            AsciiEscapes.Green, AsciiEscapes.Default))
+    tagged_image_name = '{user}/{image}:{tag}'.format(
+        user=docker_hub_user, image=imagename, tag=imagetag)
+    subprocess.check_call([
+            'docker','build','--disable-content-trust=false','-t',
+            tagged_image_name,build_dir])
+
+def docker_push(imagename, imagetag, build_dir): 
+    print('{}Pushing docker image...{}'.format(
+            AsciiEscapes.Green, AsciiEscapes.Default))
+    tagged_image_name = '{user}/{image}:{tag}'.format(
+        user=docker_hub_user, image=imagename, tag=imagetag)
+    subprocess.check_call([
+            'docker','push','--disable-content-trust=false',
+            tagged_image_name])
+
+def show_docker_commands(imagename, imagetag, build_dir):
+
+    docker_cmds  = "docker build --disable-content-trust=false -t {user}/{image}:{tag} {builddir}"
+    docker_cmds += "\ndocker push --disable-content-trust=false  {user}/{image}:{tag}"
     
+    print("########")
+    print("# Build & Push :{tag}".format(tag=imagetag))
+    print(docker_cmds.format(
+            user=docker_hub_user, image=imagename, tag=imagetag, builddir=build_dir))
+    print("########")
+    print("# Build & Push :latest")
+    print(docker_cmds.format(
+            user=docker_hub_user, image=imagename, tag="latest", builddir=build_dir))
+    print("########")
 
 def dmake_main(*args): 
     argparser = argparse.ArgumentParser(description='This is some dumb shit')
-    argparser.add_argument('dockername', action='store',
-        help='The name of the docker configuration to build')
+    argparser.add_argument(
+        'name', action='store',
+        help='The name of the docker image to build')
+    argparser.add_argument(
+        'tag', action='store',
+        help='The tag for this version of the docker image')
+    argparser.add_argument(
+        '--build', action='store_true',
+        help='Build the docker image')
+    argparser.add_argument(
+        '--push', action='store_true',
+        help='Push the docker image to the registry. (Implies --build.)')
+    argparser.add_argument(
+        '--no-latest','-l', action='store_true',
+        help='Do not build/push the :latest tag, just the named tag')
+    
     parsedargs = argparser.parse_args()
 
-    src_dir = os.path.abspath("{}/{}".format(script_root, parsedargs.dockername))
+    src_dir = os.path.abspath("{}/{}".format(script_root, parsedargs.name))
     if not os.path.exists(src_dir):
-        raise Exception("No such docker name '{}'".format(parsedargs.dockername))
-    build_dir = os.path.abspath("{}/build/{}".format(script_root, parsedargs.dockername))
-    build_docker_dir(parsedargs.dockername, build_dir)
+        raise Exception("No such docker name '{}'".format(parsedargs.name))
+    build_dir = os.path.abspath("{}/build/{}".format(script_root, parsedargs.name))
+    populate_build_dir(parsedargs.name, build_dir)
+    if parsedargs.build or parsedargs.push: 
+        docker_build(parsedargs.name, parsedargs.tag, build_dir)
+        if not parsedargs.no_latest:
+            docker_build(parsedargs.name, 'latest', build_dir)
+    if parsedargs.push:
+        docker_push(parsedargs.name, parsedargs.tag, build_dir)
+        if not parsedargs.no_latest:
+            docker_push(parsedargs.name, 'latest', build_dir)
+    show_docker_commands(parsedargs.name, parsedargs.tag, build_dir)
 
 if __name__ == '__main__':
     sys.exit(dmake_main(*sys.argv))
--- readme.markdown
+++ readme.markdown
@@ -45,7 +45,18 @@ When creating new images:
     docker build --tag=$hub_username/$image_name:$image_version build/$image_name/
     
     # 4. Push it
-    docker push $hub_username/$image_name
+    # If you've never pushed an image protected by Docker Content Trust before, it will ask you for a new root key passphrase
+    # If you've never pushed an image to $hub_username/$image_name before, it will ask you for a new repo key passphrase
+    # NOTE: if you omit the :$image_version part (what Docker calls the "tag"), Docker will *not* sign your image
+    # This is because Docker Content Trust is designed to operate only on tags. If you want to use DCT, you must use tags
+    docker push $hub_username/$image_name:$image_version
+    
+    # 5. Back up your certs and keys
+    tar -C ~/.docker/trust/private -c root_keys > ~/root_keys.tar
+    # ... now encrypt that backup and delete the tarfile ...
+    # Note that this just gets your DCT root key - not your repo keys. 
+    # (See <https://docs.docker.com/engine/security/trust/trust_key_mng/>)
+    # To get your repo keys, back up .docker/trust/private/tuf_keys in addition to root_keys
     
 When consuming images: 
 
@@ -56,7 +67,12 @@ When consuming images:
     DOCKER_CONTENT_TRUST=true
 
     # 2. Pull the image
+    # NOTE: this does NOT ASK YOU if you want to trust it -- it AUTOMATICALLY trusts the first fucking key it's given. Christ.
     docker pull $hub_username/$image_name
+    
+NOTES: 
+
+- If you can't figure out why it's not trying to use Trusted Content, you can force it with `docker <COMMAND> --disable-content-trust=false` (lol yes, that seems backwards to me too)
     
 REMAINING QUESTIONS: 
 
