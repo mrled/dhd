573e60c2f5dcad863d0a740fb0dd0dc25ce98264
Micah R Ledbetter <mrled@younix.us>
2016-06-16 11:07:59 -0500

    Improve python cli template

--- opt/python/cli.template.py
+++ opt/python/cli.template.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import subprocess
 import sys
+
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+debug = False
 
 
 def strace():
     import pdb
     pdb.set_trace()
+
+
+def debugprint(message):
+    global debug
+    if debug:
+        print(message)
+
+
+def echo(message):
+    result = subprocess.call(['/bin/echo', message])
+    return result
 
 
 def whatever(something):
@@ -17,10 +34,16 @@ def main(*args, **kwargs):
     parser = argparse.ArgumentParser(
         description="A template for writing a new Python3 command line tool")
     parser.add_argument(
+        "-d", action='store_true', dest='debug',
+        help="Include debugging output")
+    parser.add_argument(
         "directobject",
         default="mom", nargs='?',
         help="The object the command is working on")
     parsed = parser.parse_args()
+    if parsed.debug:
+        global debug
+        debug = True
     whatever(parsed.directobject)
 
 
