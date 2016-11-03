3b6dba98d7ce467543e9baba6264355bbf03d058
Micah R Ledbetter <micah@doublelinepartners.com>
2016-11-03 18:08:51 -0500

    Configure NEDOE VPN in Freyja provisioner

--- windows-trial-lab/scripts/freyja-provisioner.ps1
+++ windows-trial-lab/scripts/freyja-provisioner.ps1
@@ -3,13 +3,20 @@
 Provision the 'Freyja' boxes, which I use for DLP
 #>
 
+$ErrorActionPreference = "Stop"
+
 Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/mrled/dhd/master/opt/powershell/magic.ps1 | Invoke-Expression
 
 # Arkansas VPN:
 $akVpnName = "Arkansas"
-if (-not (Get-VpnConnection -Name $akVpnName)) {
+if (-not (Get-VpnConnection -Name $akVpnName -ErrorAction SilentlyContinue)) {
     Add-VpnConnection -Name Arkansas -ServerAddress vpn.dis.state.ar.us -TunnelType PPTP -EncryptionLevel Optional -AuthenticationMethod MSChapv2 -RememberCredential
     # [-SplitTunneling] [-EapConfigXmlStream <xml>]
+}
+
+$neVpnName = "Nebraska"
+if (-not (Get-VpnConnection -Name $neVpnName -ErrorAction SilentlyContinue)) {
+    Add-VpnConnection -Name Nebraska -ServerAddress 162.127.2.14 -TunnelType Automatic -EncryptionLevel Optional -AuthenticationMethod MSChapv2 -RememberCredential
 }
 
 # MSDF VPN software (unconfigured):
