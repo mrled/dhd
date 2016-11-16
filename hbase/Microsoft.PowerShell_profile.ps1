afb801de0fbb517b4b456940f26727b5ba14b35e
Micah R Ledbetter <micah@doublelinepartners.com>
2016-11-16 12:46:37 -0600

    Further clean up of Microsoft.Powershell_profile.ps1

--- hbase/Microsoft.PowerShell_profile.ps1
+++ hbase/Microsoft.PowerShell_profile.ps1
@@ -9,20 +9,13 @@ My Powershell profile
 
 $ErrorActionPreference = "Stop"
 
-if (-not $profile) {
-    # This can happen in a remote session, for example
-    $profile = New-Object PSObject -Property @{
-        CurrentUserCurrentHost = "$Home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-    }
-    Add-Member -InputObject $profile -MemberType ScriptMethod -Name ToString -Value {$this.CurrentUserCurrentHost} -Force
+$newProfileProperties = @{
+    ProfileD = "$Home\.dhd\opt\powershell\profile.d"
+    MrlFormat = "$Home\.dhd\opt\powershell\mrl.format.ps1xml"
+    DHDProfile = $MyInvocation.MyCommand.Path
+    ConEmu =  "$env:AppData\ConEmu.xml"
 }
-
-Add-Member -InputObject $profile -MemberType NoteProperty -Name "ProfileD" -Value "$home\.dhd\opt\powershell\profile.d" -Force
-
-# $profile is actually a PS object. $profile|get-member shows other NoteProperty entries that may be of interest
-# After this line you can do $profile.DHDProfile to get the path to this file.
-$profile | Add-Member -MemberType NoteProperty -Name "DHDProfile" -Value $myinvocation.mycommand.path -force
-$profile | Add-Member -MemberType NoteProperty -Name "ConEmu" -Value "$env:AppData\ConEmu.xml" -force
+Add-Member -NotePropertyMembers $newProfileProperties -InputObject $profile -Force
 
 $PossibleModulePaths = @(
     "${env:programfiles(x86)}\Windows Kits\8.0\Assessment and Deployment Kit\Deployment Tools\${env:Processor_Architecture}\DISM"
@@ -31,9 +24,7 @@ $PossibleModulePaths = @(
     "$home\.dhd\opt\powershell\modules"
     "$home\Documents\WindowsPowerShell\Modules"
 )
-foreach ($pmp in $PossibleModulePaths) {
-    if (test-path $pmp) { $env:PSModulePath += ";$pmp" }
-}
+$PossibleModulePaths |% {if (Test-Path $_) {$env:PSModulePath += ";$_"}}
 
 $modules = @(
     # Third party modules:
@@ -53,8 +44,7 @@ Get-ChildItem $profile.ProfileD |% { . $_.FullName }
 
 # Note that PSCX fucks with my get-childitem formatting in my mrl.format.ps1xml file, 
 # so if you're going to use that module, import it first so my format file overrides their bullshit
-Add-Member -force -inputObject $profile -MemberType NoteProperty -Name MrlFormat -Value "$Home\.dhd\opt\powershell\mrl.format.ps1xml"
-Update-FormatData -prependpath $profile.MrlFormat
+Update-FormatData -Prependpath $profile.MrlFormat
 
 Set-UserPrompt
 
