136689c9816ddd237e17f17241a32fdf7f650bed
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-01-10 17:15:36 -0600

    Remove dlp.ps1, add repohelper.ps1

---
+++ opt/powershell/profile.d/repohelper.ps1
@@ -0,0 +1,10 @@
+# This is mostly just useful because Windows *still* fucking suxxx in 2017 and will throw a fit if you have long paths. Fine, you piece of shit, I'll use a fake drive. Christ.
+$repoBase = "~/Documents/Repositories"
+if (Test-Path $repoBase) {
+    $repoBase = Resolve-Path $repoBase | Select-Object -ExpandProperty Path
+    $repoDrive = "R"
+    if (-not (Get-PsDrive | Where-Object -Property Name -EQ $repoDrive)) {
+        subst.exe "${repoDrive}:" $repoBase
+        Get-PsDrive | Out-Null # Apparently Powershell won't notice the new drive unless you do this
+    }
+}
