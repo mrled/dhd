80d24740270e86fee4e2e00d2423af103247416f
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-05-10 14:56:54 -0500

    Improve cli.template.py
    - Use logging module
    - No debug global var bullshit

--- opt/python/cli.template.py
+++ opt/python/cli.template.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import os
 import subprocess
 import sys
 
 
 scriptdir = os.path.dirname(os.path.realpath(__file__))
-debug = False
 
 
 # Helper functions
 
-
 def strace():
     import pdb
     pdb.set_trace()
-
-
-def debugprint(message):
-    global debug
-    if debug:
-        print(message)
 
 
 def resolvepath(path):
@@ -35,27 +28,30 @@ def which(commandname):
         path = path.strip('"')
         exe_file = os.path.join(path, commandname)
         if is_exe(exe_file):
+            logging.info("Exe path is {}".format(exe_file))
             return exe_file
-    print("Exe path is {}".format(exe_file))
-    raise Exception("No such command '{}' in %PATH%".format(commandname))
+    logging.error("Could not find {} in PATH, which includes:\n{}".format(
+        commandname, "\n".join(os.environ["PATH"].split(os.pathsep))))
+    raise Exception("No such command '{}' in PATH".format(commandname))
 
 
-# Example functions
-
+# Example implementation functions
 
 def echo(message):
+    logging.debug("Asked to echo a message: {}".format(message))
     result = subprocess.call(['/bin/echo', message])
     return result
 
 
-# Implementation functions
-
-
 def whatever(something):
+    logging.debug("I'm doing whatever with {}".format(something))
     print("yeah ok whatever, {}!!".format(something))
 
 
+# Main handling
+# The main() function is not special - it's invoked explicitly at the end
 def main(*args, **kwargs):
+    logging.basicConfig()
     parser = argparse.ArgumentParser(
         description="A template for writing a new Python3 command line tool")
     parser.add_argument(
@@ -67,10 +63,11 @@ def main(*args, **kwargs):
         help="The object the command is working on")
     parsed = parser.parse_args()
     if parsed.debug:
-        global debug
-        debug = True
+        logging.root.setLevel(logging.DEBUG)
     whatever(parsed.directobject)
 
 
+# Unless we are running this script directly on the commandline, the main()
+# function will NOT execute
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
