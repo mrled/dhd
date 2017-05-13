4fb60558ced402fcd92bbf10ee7a271fa69e6641
Micah R Ledbetter <me@micahrl.com>
2017-05-13 14:45:02 -0500

    Improve template.sh, and add more notes on POSIX compliance

--- opt/bin/template.sh
+++ opt/bin/template.sh
@@ -2,23 +2,45 @@
 
 set -e  # Exit immediately if a command files
 set -u  # Treat unset variables as errors
-set -o pipefail  # If a piped command fails, fail the whole pipeline
 
 usage() {
     cat << ENDUSAGE
 $0 [-h|--help]
-A template for new shell scripts
-    -h: Print help and exit
+A template for new POSIX shell scripts
+    -h | --help: Print help and exit
+    -d | --debug: show debug messages
+    -1 | --one-argument: consume the next input argument
+    -2 | --two-arguments: consume the next two input arguments
+    -e | --exit-with-error: exit with an error
+    positional arguments: printed at the end
 
 **IN GENERAL, PLEASE DO NOT WRITE NEW SHELL SCRIPTS**
 Shell scripts are only appropriate if:
 - The script will only be run in a POSIX sh environment
   (typically Unix, although possibly mingw)
-- The script can be written for POSIX sh, with no bash-isms
-  (these indicate it should be written in a better, more powerful language)
+- The script can be written with no bashisms or other non-POSIX extensions
 - The script is less than one single solitary PgDn
 - The script deals heavily with shell issues
-- The script does not rely on third party external tools
+- The script does not rely on non-POSIX third party commands
+
+Some notes
+- Why no bashisms? Portability
+  Under many circumstances, bash is not /bin/sh, and may not even be installed
+  For example: alpine linux and Ubuntu ship the Almquist shell (ash) as /bin/sh
+  A script that requires bashisms is a script that should be rewritten in a
+  more powerful and more readable programming language
+- 'set -o pipefile' is a bashism, and not a stanard POSIX option
+  In bash, that will fail the whole pipeline if a piped command exits with a
+  nonzero return value
+  However, that is not ideal because a command may exit with a nonzero return
+  value intentionally based on the command it is piped to
+  For instance, the 'cat' command in 'cat /etc/profile | head 1' exits with a
+  nonzero return value because 'head' sends SIGPIPE to 'cat'
+  See also http://cfajohnson.com/shell/cus-faq-2.html#Q11
+- Variable capitalization:
+  Exported variables should be in all caps ('export VAR=value')
+  Script local variables should be in lower case
+  See also http://stackoverflow.com/questions/673055/correct-bash-and-shell-script-variable-capitalization
 ENDUSAGE
 }
 
@@ -60,6 +82,10 @@ while [ $ctr -lt $# ]; do
             twoarg2=$3
             ctr=$((ctr+3))
             shift 3
+            ;;
+        -e | --exit-with-error )
+            # When exiting for an error, use a value greater than zero
+            exit 1
             ;;
         *)
             ctr=$((ctr+1))
