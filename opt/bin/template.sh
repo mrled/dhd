8d2c8637a0ea19fcf1300eb4f5226f6bd5f30adc
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-05-30 15:05:37 -0500

    Improve template.sh with more documentation and examples

--- opt/bin/template.sh
+++ opt/bin/template.sh
@@ -7,15 +7,20 @@ TEMPLATEDBG=${TEMPLATEDBG:-}
 set -u  # Treat unset variables as errors
 
 usage() {
-    cat << ENDUSAGE
+    cat <<ENDUSAGE
 Usage: $0 [-h] [-d] [-1 arg1] [-2 arg1 arg2] [-e] [POSITIONALARGS...]
 A template for new POSIX shell scripts
+
+ARGUMENTS
     -h | --help: Print help and exit
     -d | --debug: show debug messages
     -1 | --one-argument: consume the next input argument
     -2 | --two-arguments: consume the next two input arguments
     -e | --exit-with-error: exit with an error
     POSITIONALARGS: printed at the end
+
+VARIABLES
+    \$TEMPLATEDBG: If set, print debugging info whether or not -d was passed
 
 **IN GENERAL, PLEASE DO NOT WRITE NEW SHELL SCRIPTS**
 Shell scripts are only appropriate if:
@@ -31,7 +36,7 @@ Some notes
   For example: Alpine Linux and Ubuntu ship the Almquist shell (ash) as /bin/sh
   A script that requires bashisms is a script that should be rewritten in a
   more powerful and more readable programming language
-- 'set -o pipefile' is a bashism, and not a stanard POSIX option
+- 'set -o pipefile' should be avoided even if it weren't a bashism
   In bash, that will fail the whole pipeline if a piped command exits with a
   nonzero return value
   However, that is not ideal because a command may exit with a nonzero return
@@ -46,7 +51,8 @@ Some notes
 - Rely on the PATH variable
   Do not assume that binaries are necessarily inside /bin or /usr/bin
   For instance, Alpine Linux has /bin/true and /bin/false, but macOS has
-  /usr/bin/true and /usr/bin/false
+  /usr/bin/true and /usr/bin/false. POSIX does not guarantee the location of
+  binaries!
 - Refer to the POSIX documentation when writing scripts
   Official shell documentation can be found here:
   http://pubs.opengroup.org/onlinepubs/9699919799/
@@ -114,22 +120,35 @@ while test $# -gt 0; do
             exit 1
             ;;
         *)
-            # NOTE: This will make arg splitting later harder.
+            # You should process each positional argument as it comes in here.
+            # It is tempting to collect them into a variable like $posargs, and
+            # that can work if you understand $IFS. However, it's ugly and non-
+            # obvious; I think simply dealing with each positional argument as
+            # it is processed makes the most sense.
             echo "Positional argument: $1"
             shift
             ;;
     esac
 done
 
+# An example of an alternative way to process arguments
+# This way is useful for scripts that don't need option flag arguments
+altargproc() {
+    # NOTE: when processing arguments this way, make sure to quote "$@". Unless
+    #       it is quoted, sh will split unquoted arguments on spaces.
+    #       For instance, say the script is called like this:
+    #         altargproc "one" "two three"
+    #       If $@ is unquoted, the for loop will iterate THREE times, with the
+    #       $arg variable as "one", "two", and "three".
+    #       However, if "$@" is quoted, the loop will iterate TWO times, with the
+    #       $arg variable as "one" and "two three".
+    echo "Alternative argument processing example:"
+    for arg in "$@"; do
+        echo "- $arg banana"
+    done
+}
+
 if test $TEMPLATEDBG; then echo "Debug mode enabled"; else echo "Debug mode disabled"; fi
 dbgecho "Debug messages only get printed if debug mode is enabled"
-echo "Passed $# arguments"
 echo "oneargval = $oneargval, twoarg1 = $twoarg1, twoarg2 = $twoarg2"
-
-# NOTE: unless the $@ array is quoted in the for loop, sh will split quoted
-# arguments on spaces. For instance, say the script is called like this:
-#   script.sh "one" "two three"
-# If $@ is unquoted, the for loop will iterate THREE times, with the $arg
-# variable as "one", "two", and "three".
-# However, if "$@" is quoted, the loop will iterate TWO times, with the $arg
-# variable as "one" and "two three".
+altargproc "one" "two three"
