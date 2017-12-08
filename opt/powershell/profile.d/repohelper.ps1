d30da9dd4be10c73135714cd9995a46d0731d692
Micah R Ledbetter <me@micahrl.com>
2017-12-08 15:24:26 -0600

    Modify repohelper to only work on Windows

--- opt/powershell/profile.d/repohelper.ps1
+++ opt/powershell/profile.d/repohelper.ps1
@@ -1,6 +1,6 @@
 # This is mostly just useful because Windows *still* fucking suxxx in 2017 and will throw a fit if you have long paths. Fine, you piece of shit, I'll use a fake drive. Christ.
 $repoBase = "~/Documents/Repositories"
-if (Test-Path $repoBase) {
+if (((Get-PowershellPlatform) -match "Win32NT") -and (Test-Path -Path $repoBase)) {
     $repoBase = Resolve-Path $repoBase | Select-Object -ExpandProperty Path
     $repoDrive = "R"
     if (-not (Get-PsDrive | Where-Object -Property Name -EQ $repoDrive)) {
