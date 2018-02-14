09e0e519c5f30d6f44d5a9d143019d5272f462d6
Micah R Ledbetter <mledbetter@certicasolutions.com>
2018-02-13 19:36:25 -0600

    Configure VS Code and Sublime
    - Handle environment variables for Script blocks
    - Symlink VS Code User directory
    - Symlink Sublime User dirctory
    - Install VS Code packages
    - Install Sublime Package Control

--- opt/workstation/win32/dscConfiguration.ps1
+++ opt/workstation/win32/dscConfiguration.ps1
@@ -11,6 +11,9 @@ Note that this *should not* do anything that requires a restart, since we call i
 Configuration DhdConfig {
     param(
         [string[]] $ComputerName = "localhost",
+        [string] $UserProfile = "${env:USERPROFILE}",
+        [string] $AppData = "${env:AppData}",
+        [string] $VsCodePath = "${env:ProgramFiles}\Microsoft VS Code\bin\code.cmd",
         [Parameter(Mandatory)] [PSCredential] $Credential
     )
 
@@ -20,8 +23,8 @@ Configuration DhdConfig {
     Node $ComputerName {
 
         File EnableWindowsPowershellProfile {
-            DestinationPath = "$env:USERPROFILE\Documents\WindowsPowershell\profile.ps1"
-            SourcePath = "$env:USERPROFILE\.dhd\hbase\Microsoft.PowerShell_profile.win32.ps1"
+            DestinationPath = "$UserProfile\Documents\WindowsPowershell\profile.ps1"
+            SourcePath = "$UserProfile\.dhd\hbase\Microsoft.PowerShell_profile.win32.ps1"
             Ensure = "Present"
             Type = "File"
             Credential = $Credential
@@ -30,8 +33,8 @@ Configuration DhdConfig {
             MatchSource = $true
         }
         File EnablePowershellCoreProfile {
-            DestinationPath = "$env:USERPROFILE\Documents\Powershell\profile.ps1"
-            SourcePath = "$env:USERPROFILE\.dhd\hbase\Microsoft.PowerShell_profile.win32.ps1"
+            DestinationPath = "$UserProfile\Documents\Powershell\profile.ps1"
+            SourcePath = "$UserProfile\.dhd\hbase\Microsoft.PowerShell_profile.win32.ps1"
             Ensure = "Present"
             Type = "File"
             Credential = $Credential
@@ -41,9 +44,62 @@ Configuration DhdConfig {
         }
 
         cMrlFileLink SymlinkKnownHosts {
-            LinkPath = "$env:USERPROFILE\.ssh\known_hosts"
-            LinkTarget = "$env:USERPROFILE\.dhd\hbase\known_hosts"
+            LinkPath = "$UserProfile\.ssh\known_hosts"
+            LinkTarget = "$UserProfile\.dhd\hbase\known_hosts"
             Ensure = "Present"
+        }
+
+        cMrlFileLink SymlinkVsCodeConfig {
+            LinkPath = "$AppData\Code\User"
+            LinkTarget = "$UserProfile\.dhd\opt\vscodeuser"
+            Ensure = "Present"
+        }
+
+        Script InstallVsCodePackages {
+            GetScript = { return @{ Result = "" } }
+            TestScript = { return $false }
+            SetScript = {
+                $extensionsTxtPath = "$using:AppData\Code\User\extensions.txt"
+                if (Test-Path -LiteralPath $extensionsTxtPath) {
+                    $extensions = Get-Content -LiteralPath $extensionsTxtPath
+                    foreach ($extension in $extensions) {
+                        Write-Verbose -Message "Installing VS Code extension '$extension'..."
+                        & $using:VsCodePath --install-extension "$extension"
+                    }
+                }
+            }
+        }
+
+        # This will save the list to the extensions.txt file, ready to be checked in to Git
+        # Not useful for new boxes, only useful when we apply this DSC config to boxes I've been using
+        # Does not commit this to Git, you have to do that yourself
+        Script SaveVsCodePackages {
+            GetScript = { return @{ Result = "" } }
+            TestScript = { return $false }
+            SetScript = {
+                $extensionsTxtPath = "$using:AppData\Code\User\extensions.txt"
+                & $using:VsCodePath --list-extensions | Out-File -LiteralPath $extensionsTxtPath -Encoding utf8
+            }
+        }
+
+        cMrlFileLink SymlinkSublimeConfig {
+            LinkPath = "$AppData\Sublime Text 3\Packages\User"
+            LinkTarget = "$UserProfile\.dhd\opt\sublimetextuser"
+            Ensure = "Present"
+        }
+
+        # On first startup, Package Control will then run and install all my packages, nice
+        Script InstallSublimeTextPackageControl {
+            GetScript = { return @{ Result = "" } }
+            TestScript = {
+                Test-Path -LiteralPath "$using:AppData\Sublime Text 3\Installed Packages\Package Control.sublime-package"
+            }
+            SetScript = {
+                $pkgUri = "https://packagecontrol.io/Package%20Control.sublime-package"
+                $pkgPath = "$using:AppData\Sublime Text 3\Installed Packages\Package Control.sublime-package"
+                New-Item -Type Directory -Force -Path $(Split-Path -LiteralPath $pkgPath) | Out-Null
+                Invoke-WebRequest -Uri $pkgUri -OutFile $pkgPath
+            }
         }
 
     }
