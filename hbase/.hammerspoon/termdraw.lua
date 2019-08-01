fe357532b7fade9d25e2c14dab60ae9c0a5b5a5a
Micah R Ledbetter <me@micahrl.com>
2019-07-31 19:59:14 -0500

    hammerspoon: Refuse to create tiny iTerm windows

--- hbase/.hammerspoon/termdraw.lua
+++ hbase/.hammerspoon/termdraw.lua
@@ -1,43 +1,31 @@
 local rectanglePreviewColor = '#81ecec'  -- cyan
-
+local rectangleCancelColor = '#f09d9d'   -- red
+local rectangleFadeDuration = 0.75
 local rectanglePreview = hs.drawing.rectangle(
  hs.geometry.rect(0, 0, 0, 0)
 )
-rectanglePreview:setStrokeWidth(2)
-rectanglePreview:setStrokeColor({ hex=rectanglePreviewColor, alpha=1 })
-rectanglePreview:setFillColor({ hex=rectanglePreviewColor, alpha=0.5 })
-rectanglePreview:setRoundedRectRadii(2, 2)
-rectanglePreview:setStroke(true):setFill(true)
-rectanglePreview:setLevel('floating')
 
 local function setRectangle(color)
+ rectanglePreview:setStrokeColor({ hex=color, alpha=1 })
+ rectanglePreview:setFillColor({ hex=color, alpha=0.5 })
+ rectanglePreview:setStrokeWidth(2)
+ rectanglePreview:setRoundedRectRadii(2, 2)
+ rectanglePreview:setStroke(true):setFill(true)
+ rectanglePreview:setLevel('floating')
 end
 
-
-local function openIterm()
- local frame = rectanglePreview:frame()
- local framedesc = string.format(
-    "{%i, %i, %i, %i} (w: %i, h: %i)",
-    frame.x, frame.y, frame.x + frame.w, frame.y + frame.h, frame.w, frame.h)
- if frame.w < 300 or frame.h < 150 then
-    -- sometimes you press the keys accidentally and invisible terms get created
-    -- this prevents that
-    hs.printf("Refusing to create tiny iTerm with " .. framedesc)
- else
-   local createItermWithBounds = string.format([[
-     if application "iTerm" is not running then
-       activate application "iTerm"
-     end if
-     tell application "iTerm"
-       set newWindow to (create window with default profile)
-       set the bounds of newWindow to {%i, %i, %i, %i}
-     end tell
-   ]], frame.x, frame.y, frame.x + frame.w, frame.y + frame.h)
-   hs.printf("Created new iTerm with " .. framedesc)
-   hs.osascript.applescript(createItermWithBounds)
- end
+local function openIterm(bounds)
+ local openItemScript = string.format([[
+   if application "iTerm" is not running then
+     activate application "iTerm"
+   end if
+   tell application "iTerm"
+     set newWindow to (create window with default profile)
+     set the bounds of newWindow to {%i, %i, %i, %i}
+   end tell
+ ]], bounds.xmin, bounds.ymin, bounds.xmax, bounds.ymax)
+ hs.osascript.applescript(openItemScript)
 end
-
 
 local fromPoint = nil
 
@@ -57,7 +45,6 @@ local drag_event = hs.eventtap.new(
  end
 )
 
-
 local flags_event = hs.eventtap.new(
  { hs.eventtap.event.types.flagsChanged },
  function(e)
@@ -65,14 +52,32 @@ local flags_event = hs.eventtap.new(
    if flags.ctrl and flags.shift then
      fromPoint = hs.mouse.getAbsolutePosition()
      local newFrame = hs.geometry.rect(fromPoint.x, fromPoint.y, 0, 0)
+     setRectangle(rectanglePreviewColor)
      rectanglePreview:setFrame(newFrame)
      drag_event:start()
      rectanglePreview:show()
+
    elseif fromPoint ~= nil then
+     local frame = rectanglePreview:frame()
+     local bounds = {xmin=frame.x, ymin=frame.y, xmax=frame.x + frame.w, ymax=frame.y + frame.h}
+     local framedesc = string.format(
+       "{%i, %i, %i, %i} (w: %i, h: %i)",
+     frame.x, frame.y, frame.x + frame.w, frame.y + frame.h, frame.w, frame.h)
+
      fromPoint = nil
      drag_event:stop()
-     rectanglePreview:hide(1)
-     openIterm()
+
+     if frame.w < 300 or frame.h < 150 then
+       -- sometimes you press the keys accidentally and invisible terms get created
+       -- this prevents that
+       setRectangle(rectangleCancelColor)
+       hs.printf("Absolutely not. Refusing to create tiny iTerm with " .. framedesc)
+     else
+       openIterm(bounds)
+       hs.printf("Created new iTerm with " .. framedesc)
+     end
+
+     rectanglePreview:hide(rectangleFadeDuration)
    end
    return nil
  end
