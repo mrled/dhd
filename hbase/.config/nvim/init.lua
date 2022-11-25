4cc6d608341a2494fbd7f699da154dc0d2bb697b
Micah R Ledbetter <me@micahrl.com>
2022-11-25 16:22:17 -0600

    neovim: Add more IDE configuration, fix fucking color scheme

--- hbase/.config/nvim/init.lua
+++ hbase/.config/nvim/init.lua
@@ -3,6 +3,16 @@
 -- Put stuff that doesn't require plugins at the top
 -- If plugins aren't working, at least the first part will load
 
+-- Reminders/docs/notes-to-self
+--
+-- 1. In vimscript, 0 is falsy and every other number is truthy since there is no boolean.
+--    In Lua, only false and nil express to false, anything else expresses to true (including 0).
+--    !!!!!!! ZERO IS TRUE IN LUA, FOR FUCK'S SAKE !!!!!!!
+--    Take special care with translating eg "if has(...)" in vimscript,
+--    which is a vimscript-native function that returns 0 or 1,
+--    to "if vim.fn.has(...)" in lua, because if has() returns 0, lua will interpret it as true.
+--    Many such cases.
+
 -- case insensitive searches
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -32,8 +42,14 @@ vim.opt.clipboard = "unnamed,unnamedplus"
 
 require('plugins')
 
-if vim.fn.has('gui_running') then
-  vim.cmd "colorscheme edge"
+-- neovim GUIs are... fucking annoying about how to support GUI-only configuration.
+-- gvim looks for a ginit.vim, but that isn't consitent with all frontends,
+-- eg vimr doesn't support ginit.<anything>, and neovide supports ginit.vim but not ginit.lua;
+-- vimr sets has('gui_running') to 1, but neovide doesn't...
+-- so we have to do this.
+-- Sad!
+if vim.fn.has('gui_running') == 1 or vim.g.neovide then
+  vim.cmd("colorscheme edge")
 end
 
 if vim.g.neovide then
@@ -55,7 +71,79 @@ end
 local builtin = require('telescope.builtin')
 -- hidden=true means look in .hidden directories too
 vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden=true}) end)
+vim.keymap.set('n', '<leader>fa', function() builtin.find_files({hidden=true}) end)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local lsp_opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, lsp_opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, lsp_opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, lsp_opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, lsp_opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local lsp_on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {}
+
+-- order is important for these:
+require("mason").setup()
+require("mason-lspconfig").setup()
+-- these can be in any order:
+require("lspconfig").ansiblels.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").bashls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").cssls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").dockerls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").jsonls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").marksman.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").salt_ls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").sumneko_lua.setup {
+  on_attach=lsp_on_attach,
+  flags=lsp_flags,
+  settings = {
+      Lua = {
+          diagnostics = {
+              globals = {
+                  'vim'
+              }
+          }
+      }
+  }
+}
+--require("lspconfig").tailwindcss.setup {}
+require("lspconfig").terraformls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+require("lspconfig").yamlls.setup {on_attach=lsp_on_attach, flags=lsp_flags}
+
+
+-- python
+-- markdown
+-- javascript/typescript ?
+-- html
+-- terraform
 
--- hbase/.config/nvim/lua/plugins.lua
+++ hbase/.config/nvim/lua/plugins.lua
@@ -1,5 +1,13 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+-- run :PackerCompile whenever this file changes
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -13,5 +21,10 @@ return require('packer').startup(function(use)
 
   use {'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { {'nvim-lua/plenary.nvim'} }}
 
+  use "vim-airline/vim-airline"
+
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+  use "neovim/nvim-lspconfig"
 end)
 
