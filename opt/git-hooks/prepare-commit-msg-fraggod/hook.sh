1addcd7793f9506169850dabd0f6dc912d69f490
Micah R Ledbetter <me@micahrl.com>
2023-07-31 16:12:06 -0500

    Improve fragged hook

--- opt/git-hooks/prepare-commit-msg-fraggod/hook.sh
+++ opt/git-hooks/prepare-commit-msg-fraggod/hook.sh
@@ -2,7 +2,7 @@
 msg_file=$1
 commit_src=$2
 hash=$3
-test -z "$msg_file" || "$GIT_EDITOR" = : && exit 0
+test -z "$msg_file" || test "$GIT_EDITOR" = ":" && exit 0
 test -z "$commit_src" || exit 0 # cherry-picks, merges, etc
 
 echo >>"$msg_file" '#'
@@ -11,4 +11,8 @@ echo >>"$msg_file" "#   Repo dir: $(realpath "$PWD")"
 echo >>"$msg_file" '#'
 echo >>"$msg_file" '# Last 10 log messages:'
 git log -10 --format='#   %s' >>"$msg_file"
+echo >>"$msg_file" '#'
+echo >>"$msg_file" '#'
+echo >>"$msg_file" '# git diff:'
+git diff --staged | sed 's/^/#   /' >> "$msg_file"
 
