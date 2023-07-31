23ac69b0560491c29b3cefae18d8126b2fa8640d
Micah R Ledbetter <mledbetter@indeed.com>
2023-07-31 16:12:39 -0500

    work

--- hosts/it-usa-vy0283/zshrc.sh
+++ hosts/it-usa-vy0283/zshrc.sh
@@ -3,7 +3,7 @@
 unset DOCKER_SCAN_SUGGEST
 
 # See also <https://code.corp.indeed.com/sre/sre-acceleration/decommissioner>
-decomissioner_export() {
+decommissioner_export() {
     export GITLAB_TOKEN="$(gopass cat mledbetter/gitlab_token)"
     export LDAP_USERNAME=mledbetter
     export LDAP_PASSWORD="$(gopass cat mledbetter/ldap_password)"
@@ -11,3 +11,10 @@ decomissioner_export() {
     export DD_API_KEY="$(gopass cat indeed/datadog_api_key_sre)"
     export DD_APP_KEY="$(gopass cat mledbetter/datadog_app_key)"
 }
+
+# This is for prod; see also <https://wiki.indeed.com/display/ITSYS/Vault>
+export VAULT_ADDR='https://vault.indeed.tech'
+
+
+
+
