850598f53595b0824772cad058c91c77bbecbe47
Micah R Ledbetter <me@micahrl.com>
2025-05-28 16:16:10 -0500

    Remove ancient config files

--- hbase/.conkeror/mac.js
+++
@@ -1,6 +0,0 @@
-function hide_conkeror() {
-    spawn_process_blind("/usr/bin/osascript", 
-                        ["osascript", "-e", 'tell application "System Events" to set visible of process "conkeror" to false']);
-}
-interactive("hide-conkeror", "Hide Conkeror (in Mac OS X).", hide_conkeror);
-define_key(content_buffer_normal_keymap, "M-h", "hide-conkeror");
--- hbase/.conkeror/specific-site-modes.js
+++
@@ -1,3 +0,0 @@
-require("page-modes/stackexchange.js");
-require("page-modes/xkcd.js");
-xkcd_add_title = true;
