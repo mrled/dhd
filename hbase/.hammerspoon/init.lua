bb07f71a2944de512af28076404d83257c8fd267
Micah R Ledbetter <me@micahrl.com>
2025-05-28 16:14:17 -0500

    .hammerspoon
    Remove old modalHotKey and menus

--- hbase/.hammerspoon/init.lua
+++ hbase/.hammerspoon/init.lua
@@ -36,38 +36,6 @@ hs.alert.show(hammerSpoonEmoji .. " Config Loaded")
 local shiftIt = require('shiftIt')
 shiftIt.config.animationDuration = 0
 
----
--- Swapp, my SWitcher of APPs.
----
-
-local modalHotKey = require("modalHotKey")
-local appModal = modalHotKey.new(
-  hs.hotkey.modal.new({ "cmd", "ctrl" }, "-"),
-  {
-    modalHotKey.shortcutKey { shortcutKey = 'b', appName = 'Boop', },
-    modalHotKey.shortcutKey { shortcutKey = 'c', appName = 'Google Chrome', },
-    modalHotKey.shortcutKey { shortcutKey = 'f', appName = 'Finder', },
-    modalHotKey.shortcutKey { shortcutKey = 'k', appName = 'Keychain Access', },
-    modalHotKey.shortcutKey { shortcutKey = 'n', appName = 'nvAlt', },
-    modalHotKey.shortcutKey { shortcutKey = 's', appName = 'Safari', },
-    modalHotKey.shortcutKey { shortcutKey = 't', appName = 'Terminal', },
-    modalHotKey.shortcutKey { shortcutKey = 'u', appName = 'Soulver 3', },
-    modalHotKey.shortcutKey { shortcutKey = 'v', appName = 'Visual Studio Code', },
-    modalHotKey.shortcutKey { shortcutKey = 'w', appName = 'Brave Browser', },
-    modalHotKey.shortcutKey { shortcutKey = 'x', appName = 'Firefox', },
-  },
-  hammerSpoonEmoji .. " Swapp"
-)
-
-appModal = modalHotKey.new(
-  hs.hotkey.modal.new({ "cmd", "ctrl" }, "="),
-  {
-    modalHotKey.shortcutKey { shortcutKey = 'l', actionDesc = "Lock screen", action = hs.caffeinate.lockScreen },
-    modalHotKey.shortcutKey { shortcutKey = 'r', actionDesc = "Reload Hammerspoon configuration", action = hs.reload },
-  },
-  hammerSpoonEmoji .. " Special functions"
-)
-
 hs.loadSpoon("GridCraft")
 
 local fiveMenu = {
