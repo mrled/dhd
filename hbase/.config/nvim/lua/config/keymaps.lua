bd66cecb57249cafe35095e54471ca3cbc58b535
Micah R Ledbetter <me@micahrl.com>
2026-02-09 05:25:26 -0600

    Add Harper, fix keymaps

--- hbase/.config/nvim/lua/config/keymaps.lua
+++ hbase/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,10 @@
 -- This file is automatically loaded by config.lazy
 
 -- Create a new file like :e, but relative to the directory of the currently-open file
-vim.keymap.set('n', '<leader>e', '<C-R>=expand("%:p:h") . "/" <CR>', { desc = "Edit file relative to current buffer" })
+vim.keymap.set('n', '<leader>E', '<C-R>=expand("%:p:h") . "/" <CR>', { desc = "Edit file relative to current buffer" })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
