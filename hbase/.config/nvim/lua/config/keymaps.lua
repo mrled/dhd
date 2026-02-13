734d8433663eb91bded497cab809896663c59a06
Micah R Ledbetter <me@micahrl.com>
2026-02-13 08:45:47 -0600

    Add leader-W to save without formatting

--- hbase/.config/nvim/lua/config/keymaps.lua
+++ hbase/.config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,9 @@
 -- Create a new file like :e, but relative to the directory of the currently-open file
 vim.keymap.set('n', '<leader>E', '<C-R>=expand("%:p:h") . "/" <CR>', { desc = "Edit file relative to current buffer" })
 
+-- Save without formatting
+vim.keymap.set('n', '<leader>W', '<cmd>noautocmd write<cr>', { desc = "Save without formatting" })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
