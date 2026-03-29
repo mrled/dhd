9d0495ed132e041be5aa5fde982608a0e4492e1d
Micah R Ledbetter <me@micahrl.com>
2026-03-29 12:41:40 -0500

    Use an eye for dreadnaught prompt

--- hosts/lima-dreadnaught/zshrc.sh
+++ hosts/lima-dreadnaught/zshrc.sh
@@ -3,5 +3,7 @@
 # linuxbrew ruins everything
 zsh_compinit_unsafe=yes
 
-test -e /etc/profile.d/chineseroom-prompt.sh && . /etc/profile.d/chineseroom-prompt.sh
+# test -e /etc/profile.d/chineseroom-prompt.sh && . /etc/profile.d/chineseroom-prompt.sh
+PROMPT='%F{white}%* %F{yellow}%n %F{red}Ꙩ %F{green}%~ %F{blue}$ %f'
 test -e ~/.config/aliases.sh && . ~/.config/aliases.sh
+
